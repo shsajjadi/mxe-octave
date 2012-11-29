05e2dbd764378786c19d52ae7508d1cd7a91b14a
John W. Eaton <jwe@octave.org>
2012-11-29 09:41:54 -0500

    fix nettle build

--- src/nettle.mk
+++ src/nettle.mk
@@ -20,8 +20,24 @@ define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
         --build="`config.guess`" \
-        $(ENABLE_SHARED_OR_STATIC) \
+        --enable-static --disable-shared \
         --prefix='$(PREFIX)/$(TARGET)'
     $(MAKE) -C '$(1)' -j '$(JOBS)' getopt.o getopt1.o
     $(MAKE) -C '$(1)' -j '$(JOBS)' install
+
+    if [ $(BUILD_SHARED) = yes ]; then \
+      $(INSTALL) -d '$(PREFIX)/$(TARGET)/bin'; \
+ \
+      $(MAKE_SHARED_FROM_STATIC) --ar '$(TARGET)-ar' --ld '$(TARGET)-gcc' '$(PREFIX)/$(TARGET)/lib/libnettle.a'; \
+      $(INSTALL) -m755 '$(PREFIX)/$(TARGET)/lib/libnettle.dll.a' '$(PREFIX)/$(TARGET)/lib/libnettle.dll.a'; \
+      $(INSTALL) -m755 '$(PREFIX)/$(TARGET)/lib/libnettle.dll' '$(PREFIX)/$(TARGET)/bin/libnettle.dll'; \
+      rm -f '$(PREFIX)/$(TARGET)/lib/libnettle.dll'; \
+      rm -f '$(PREFIX)/$(TARGET)/lib/libnettle.la'; \
+ \
+      $(MAKE_SHARED_FROM_STATIC) --ar '$(TARGET)-ar' --ld '$(TARGET)-gcc' '$(PREFIX)/$(TARGET)/lib/libhogweed.a' -lnettle -lgmp; \
+      $(INSTALL) -m755 '$(PREFIX)/$(TARGET)/lib/libhogweed.dll.a' '$(PREFIX)/$(TARGET)/lib/libhogweed.dll.a'; \
+      $(INSTALL) -m755 '$(PREFIX)/$(TARGET)/lib/libhogweed.dll' '$(PREFIX)/$(TARGET)/bin/libhogweed.dll'; \
+      rm -f '$(PREFIX)/$(TARGET)/lib/libhogweed.dll'; \
+      rm -f '$(PREFIX)/$(TARGET)/lib/libhogweed.la'; \
+    fi
 endef
