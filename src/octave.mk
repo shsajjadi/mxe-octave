75c747ccca01914b9a277a08144c136642031999
John W. Eaton <jwe@octave.org>
2012-12-04 13:44:38 -0500

    Include fontconfig and freetype as a dependencies of Octave. Allow configuring with opengl. Patch Octave configure script to use pkg-config to check for freetype libraries.

--- src/octave.mk
+++ src/octave.mk
@@ -7,7 +7,7 @@ $(PKG)_CHECKSUM := ff5d66986789412475d64d8ba2c30714ddfc13c4
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := ftp://gnu.org/gnu/octave/$($(PKG)_FILE)
-$(PKG)_DEPS     := arpack blas curl fftw fltk gcc glpk gnuplot graphicsmagick lapack pcre qhull qrupdate readline suitesparse zlib
+$(PKG)_DEPS     := arpack blas curl fftw fltk fontconfig freetype gcc glpk gnuplot graphicsmagick lapack pcre qhull qrupdate readline suitesparse zlib
 
 define $(PKG)_UPDATE
     echo 'Warning: Updates are temporarily disabled for package octave.' >&2;
@@ -15,14 +15,14 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    # build GCC and support libraries
+    cd '$(1)' && autoreconf
     mkdir '$(1)/.build'
     cd '$(1)/.build' && '$(1)/configure' \
         --host='$(TARGET)' \
         --build="`config.guess`" \
         --prefix='$(PREFIX)/$(TARGET)' \
-        --without-opengl \
         --disable-docs \
+        FLTK_CONFIG="$(PREFIX)/bin/$(TARGET)-fltk-config" \
         gl_cv_func_gettimeofday_clobber=no
 
     $(MAKE) -C '$(1)/.build' -j '$(JOBS)' install
