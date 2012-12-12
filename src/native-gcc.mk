15fc47b842eac0744dc333bc2ab68dd81716ba6c
John W. Eaton <jwe@octave.org>
2012-12-12 09:54:24 -0500

    native gcc and binutils

---
+++ src/native-gcc.mk
@@ -0,0 +1,94 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := native-gcc
+$(PKG)_IGNORE   :=
+$(PKG)_CHECKSUM := a464ba0f26eef24c29bcd1e7489421117fb9ee35
+$(PKG)_SUBDIR   := gcc-$($(PKG)_VERSION)
+$(PKG)_FILE     := gcc-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/gcc/gcc-$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_URL_2    := ftp://ftp.mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_DEPS     := mingwrt w32api binutils gcc-gmp gcc-mpc gcc-mpfr
+ifneq ($(BUILD_SHARED),yes)
+$(PKG)_STATIC_FLAG := --static
+endif
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://ftp.gnu.org/gnu/gcc/?C=M;O=D' | \
+    $(SED) -n 's,.*<a href="gcc-\([0-9][^"]*\)/".*,\1,p' | \
+    grep -v '^4\.[543]\.' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    # unpack support libraries
+    cd '$(1)' && $(call UNPACK_PKG_ARCHIVE,gcc-gmp)
+    mv '$(1)/$(gcc-gmp_SUBDIR)' '$(1)/gmp'
+    cd '$(1)' && $(call UNPACK_PKG_ARCHIVE,gcc-mpc)
+    mv '$(1)/$(gcc-mpc_SUBDIR)' '$(1)/mpc'
+    cd '$(1)' && $(call UNPACK_PKG_ARCHIVE,gcc-mpfr)
+    mv '$(1)/$(gcc-mpfr_SUBDIR)' '$(1)/mpfr'
+
+    # build GCC and support libraries
+    mkdir '$(1).build'
+    cd    '$(1).build' && '$(1)/configure' \
+        --target='$(TARGET)' \
+        --host='$(TARGET)' \
+        --build="`config.guess`" \
+        --prefix='/usr' \
+        --libdir='/usr/lib' \
+        --enable-languages='c,c++,objc,fortran' \
+        --enable-version-specific-runtime-libs \
+        --with-gcc \
+        --with-gnu-ld \
+        --with-gnu-as \
+        --disable-nls \
+        $(ENABLE_SHARED_OR_STATIC) \
+        --disable-sjlj-exceptions \
+        --without-x \
+        --disable-win32-registry \
+        --enable-threads=win32 \
+        --disable-libgomp \
+        --disable-libmudflap \
+        --with-mpfr-include='$(1)/mpfr/src' \
+        --with-mpfr-lib='$(1).build/mpfr/src/.libs' \
+        --with-native-system-header-dir=$(PREFIX)/$(TARGET)/include \
+        $(shell [ `uname -s` == Darwin ] && echo "LDFLAGS='-Wl,-no_pie'")
+    $(MAKE) -C '$(1).build' -j '$(JOBS)'
+    $(MAKE) -C '$(1).build' -j 1 DESTDIR='$(PREFIX)/../native' install
+
+    # # create pkg-config script
+    # (echo '#!/bin/sh'; \
+    #  echo 'PKG_CONFIG_PATH="$$PKG_CONFIG_PATH_$(subst -,_,$(TARGET))" PKG_CONFIG_LIBDIR='\''$(PREFIX)/$(TARGET)/lib/pkgconfig'\'' exec pkg-config $($(PKG)_STATIC_FLAG) "$$@"') \
+    #          > '$(PREFIX)/bin/$(TARGET)-pkg-config'
+    # chmod 0755 '$(PREFIX)/bin/$(TARGET)-pkg-config'
+
+    # # create the CMake toolchain file
+    # [ -d '$(dir $(CMAKE_TOOLCHAIN_FILE))' ] || mkdir -p '$(dir $(CMAKE_TOOLCHAIN_FILE))'
+    # (echo 'set(CMAKE_SYSTEM_NAME Windows)'; \
+    #  echo 'set(MSYS 1)'; \
+    #  if [ $(BUILD_SHARED) = yes ]; then \
+    #    echo 'set(BUILD_SHARED_LIBS ON)'; \
+    #  else \
+    #    echo 'set(BUILD_SHARED_LIBS OFF)'; \
+    #  fi; \
+    #  if [ $(BUILD_STATIC) = yes ]; then \
+    #    echo 'set(BUILD_STATIC_LIBS ON)'; \
+    #  else \
+    #    echo 'set(BUILD_STATIC_LIBS OFF)'; \
+    #  fi; \
+    #  echo 'set(CMAKE_BUILD_TYPE Release)'; \
+    #  echo 'set(CMAKE_FIND_ROOT_PATH $(PREFIX)/$(TARGET))'; \
+    #  echo 'set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)'; \
+    #  echo 'set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)'; \
+    #  echo 'set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)'; \
+    #  echo 'set(CMAKE_C_COMPILER $(PREFIX)/bin/$(TARGET)-gcc)'; \
+    #  echo 'set(CMAKE_CXX_COMPILER $(PREFIX)/bin/$(TARGET)-g++)'; \
+    #  echo 'set(CMAKE_Fortran_COMPILER $(PREFIX)/bin/$(TARGET)-gfortran)'; \
+    #  echo 'set(CMAKE_RC_COMPILER $(PREFIX)/bin/$(TARGET)-windres)'; \
+    #  echo 'set(PKG_CONFIG_EXECUTABLE $(PREFIX)/bin/$(TARGET)-pkg-config)'; \
+    #  echo 'set(QT_QMAKE_EXECUTABLE $(PREFIX)/bin/$(TARGET)-qmake)'; \
+    #  echo 'set(CMAKE_INSTALL_PREFIX $(PREFIX)/$(TARGET) CACHE PATH "Installation Prefix")'; \
+    #  echo 'set(CMAKE_BUILD_TYPE Release CACHE STRING "Debug|Release|RelWithDebInfo|MinSizeRel")') \
+    #  > '$(CMAKE_TOOLCHAIN_FILE)'
+endef
