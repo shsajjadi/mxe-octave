0c92f9a9788a31477966fe449719779dc89f8c0e
John W. Eaton <jwe@octave.org>
2012-12-19 11:53:26 -0500

    fix download url for glpk

--- src/glpk.mk
+++ src/glpk.mk
@@ -6,7 +6,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_CHECKSUM := 35e16d3167389b6bc75eb51b4b48590db59f789c
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := glpk-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/glpk/glpk/$($(PKG)_FILE)
+$(PKG)_URL      := ftp://ftp.gnu.org/gnu/glpk/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
