317b54797baa25f66c3c451beb2b70dec2a58bae
John Donoghue <john.donoghue@ieee.org>
2013-06-08 10:48:17 -0400

    Update lapack for native mingw compile
    * src/lapack.mk: add PKG_CMAKE_FLAGS options when compiling native mingw

--- src/lapack.mk
+++ src/lapack.mk
@@ -13,6 +13,10 @@ ifeq ($(ENABLE_64),yes)
   $(PKG)_DEFAULT_INTEGER_8_FLAG := -fdefault-integer-8
 endif
 
+ifeq ($(MXE_NATIVE_MINGW_BUILD),yes)
+  $(PKG)_CMAKE_FLAGS := -G 'MSYS Makefile'
+endif
+
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://www.netlib.org/lapack/' | \
     $(SED) -n 's_.*>LAPACK, version \([0-9]\.[0-9]\.[0-9]\).*_\1_ip' | \
@@ -21,6 +25,7 @@ endef
 
 define $(PKG)_BUILD
     cd '$(1)' && cmake \
+        $($(PKG)_CMAKE_FLAGS) \
         -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
         -DCMAKE_AR='$(MXE_AR)' \
         -DCMAKE_RANLIB='$(MXE_RANLIB)' \
