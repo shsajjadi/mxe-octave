572bf33cf7456a8be7af069bc1ab954abbfdb605
John Donoghue <john.donoghue@ieee.org>
2013-06-08 18:52:03 -0400

    Update pthread for native mingw build
    * src/pthreads.mk: added new build rule for native mingw

--- src/pthreads.mk
+++ src/pthreads.mk
@@ -14,6 +14,23 @@ define $(PKG)_UPDATE
     $(SED) -n 's/^#define PTW32_VERSION \([^,]*\),\([^,]*\),\([^,]*\),.*/\1-\2-\3/p;'
 endef
 
+ifeq ($(MXE_NATIVE_MINGW_BUILD),yes)
+define $(PKG)_BUILD
+    $(MAKE) -C '$(1)' -j 1 GC-static 
+    $(INSTALL) -d '$(HOST_LIBDIR)'
+    $(INSTALL) -m644 '$(1)/libpthreadGC2.a' '$(HOST_LIBDIR)/libpthread.a'
+
+    if [ $(BUILD_SHARED) = yes ]; then \
+      $(MAKE_SHARED_FROM_STATIC) --ar '$(MXE_AR)' --ld '$(MXE_CC)' '$(HOST_LIBDIR)/libpthread.a' --install '$(INSTALL)' --libdir '$(HOST_LIBDIR)' --bindir '$(HOST_BINDIR)'; \
+    fi
+
+    $(INSTALL) -d '$(HOST_INCDIR)'
+    $(INSTALL) -m644 '$(1)/pthread.h'   '$(HOST_INCDIR)'
+    $(INSTALL) -m644 '$(1)/sched.h'     '$(HOST_INCDIR)'
+    $(INSTALL) -m644 '$(1)/semaphore.h' '$(HOST_INCDIR)'
+endef
+
+else
 ifeq ($(MXE_SYSTEM),mingw)
 define $(PKG)_BUILD
     $(MAKE) -C '$(1)' -j 1 GC-static CROSS='$(TARGET)-'
@@ -32,4 +49,5 @@ endef
 else
 define $(PKG)_BUILD
 endef
+endif
 endif
