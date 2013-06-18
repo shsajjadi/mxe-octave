de569d7a393868097dff0e372cb3e7f54512a816
John Donoghue <john.donoghue@ieee.org>
2013-06-18 18:34:53 -0400

    Add QScintilla blockcomments patch
    * src/qscintilla-1-matlab-blockcomments.patch: New file
    
    * src/qscintilla.mk: change PKG_SUBDIR to base of tarball so patch works, and change PKG_BUILD to (1)/Qt4Qt5.

--- src/qscintilla.mk
+++ src/qscintilla.mk
@@ -4,7 +4,7 @@
 PKG             := qscintilla
 $(PKG)_IGNORE   :=
 $(PKG)_CHECKSUM := 2a11fb6be2c3005bc6502f929a0a339d4303af9b
-$(PKG)_SUBDIR   := QScintilla-gpl-$($(PKG)_VERSION)/Qt4Qt5
+$(PKG)_SUBDIR   := QScintilla-gpl-$($(PKG)_VERSION)
 $(PKG)_FILE     := QScintilla-gpl-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://sourceforge.net/projects/pyqt/files/QScintilla2/QScintilla-$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := qt
@@ -21,10 +21,10 @@ ifneq ($(MXE_NATIVE_BUILD),yes)
 endif
 
 define $(PKG)_BUILD
-    cd '$(1)' && '$(HOST_BINDIR)/qmake' -makefile $($(PKG)_QMAKE_SPEC_OPTION)
+    cd '$(1)/Qt4Qt5' && '$(HOST_BINDIR)/qmake' -makefile $($(PKG)_QMAKE_SPEC_OPTION)
 
-    $(MAKE) -C '$(1)' -j '$(JOBS)'
-    $(MAKE) -C '$(1)' -j 1 install
+    $(MAKE) -C '$(1)/Qt4Qt5' -j '$(JOBS)'
+    $(MAKE) -C '$(1)/Qt4Qt5' -j 1 install
     if [ $(MXE_SYSTEM) = mingw ]; then \
       $(INSTALL) -m755 '$(HOST_LIBDIR)/qscintilla2.dll' '$(HOST_BINDIR)/qscintilla2.dll'; \
     fi
