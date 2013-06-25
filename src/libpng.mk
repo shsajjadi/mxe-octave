54b4266700b67c52fb79c38cfb9ee175e9e138ba
Michael Goffioul <michael.goffioul@gmail.com>
2013-06-25 11:41:22 -0400

    [MSVC] enable libpng compilation

--- src/libpng.mk
+++ src/libpng.mk
@@ -10,6 +10,11 @@ $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)$(subst .,,
 $(PKG)_URL_2    := ftp://ftp.simplesystems.org/pub/$(PKG)/png/src/$($(PKG)_FILE)
 $(PKG)_DEPS     := zlib
 
+# Configure script detection of memset and pow doesn't work on MSVC.
+ifeq ($(MXE_SYSTEM),msvc)
+    $(PKG)_CONFIGURE_OPTIONS := ac_cv_func_memset=yes ac_cv_func_pow=yes
+endif
+
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://libpng.git.sourceforge.net/git/gitweb.cgi?p=libpng/libpng;a=tags' | \
     grep '<a class="list name"' | \
@@ -26,7 +31,8 @@ define $(PKG)_BUILD
         $(CONFIGURE_CPPFLAGS) $(CONFIGURE_LDFLAGS) \
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         $(ENABLE_SHARED_OR_STATIC) \
-        --prefix='$(HOST_PREFIX)'
+	$($(PKG)_CONFIGURE_OPTIONS) \
+        --prefix='$(HOST_PREFIX)' && $(CONFIGURE_POST_HOOK)
     $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 
     rm -f $(HOST_LIBDIR)/libpng.la
