8539449461c3fcc5764e2721779ac698848d7063
Michael Goffioul <michael.goffioul@gmail.com>
2013-06-28 19:56:00 -0400

    [MSVC] enable libmng compilation
    - discard lcms1 dependency, libmng does not support lcms2 and lcms1 does
      not compile easily with MSVC (ancient libtool version used)

--- src/libmng.mk
+++ src/libmng.mk
@@ -7,7 +7,10 @@ $(PKG)_CHECKSUM := c21c84b614500ae1a41c6595d5f81c596e406ca2
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)-devel/$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := zlib jpeg lcms1
+$(PKG)_DEPS     := zlib jpeg
+ifneq ($(MXE_SYSTEM),msvc)
+    $(PKG)_DEPS += lcms1
+endif
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://sourceforge.net/projects/libmng/files/libmng-devel/' | \
@@ -24,12 +27,12 @@ define $(PKG)_BUILD
         $(CONFIGURE_CPPFLAGS) $(CONFIGURE_LDFLAGS) \
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         --prefix='$(HOST_PREFIX)' \
-        --enable-shared
+        --enable-shared && $(CONFIGURE_POST_HOOK)
     $(MAKE) -C '$(1)/.build' -j '$(JOBS)' install
     $(SED) -e 's^@prefix@^$(HOST_PREFIX)^;' \
            -e 's^@VERSION@^$(libmng_VERSION)^;' \
            -e 's^@mng_libs_private@^-ljpeg^;' \
-           -e 's^@mng_requires_private@^lcms zlib^;' \
+           -e 's^@mng_requires_private@^lcms2 zlib^;' \
            < '$(1)/libmng.pc.in' > '$(1)/libmng.pc'
     $(INSTALL) -m644 '$(1)/libmng.pc' '$(HOST_LIBDIR)/pkgconfig/'
 endef
