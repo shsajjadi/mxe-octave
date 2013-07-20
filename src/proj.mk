20a058fafec40d8e2387286bfb8b0eeced69713c
Michael Goffioul <michael.goffioul@gmail.com>
2013-07-20 11:55:26 -0400

    [MSVC] enable PROJ compilation

--- src/proj.mk
+++ src/proj.mk
@@ -17,11 +17,16 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
+    if [ $(MXE_SYSTEM) = msvc ]; then \
+	mkdir '$(1)/m4'; \
+        cd '$(1)' && autoreconf -f -i -v; \
+    fi
     cd '$(1)' && ./configure \
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         $(ENABLE_SHARED_OR_STATIC) \
         --prefix='$(HOST_PREFIX)' \
-        --with-mutex
+        --with-mutex \
+	&& $(CONFIGURE_POST_HOOK)
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     # remove header which is not installed since 4.8.0
     rm -f '$(HOST_INCDIR)/projects.h'
