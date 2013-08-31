8d3f29928b83d986fef34ecfb46d4aea894b7211
John Donoghue <john.donoghue@ieee.org>
2013-08-30 20:54:00 -0400

    Update fftw to 3.3.3
    * index.html: update fftw-version to 3.3.3.
    
    * src/fftw.mk: update checksum for 3.3.3.
    
    * src/msvc-fftw-1.patch: update patch for 3.3.3 sources.

--- src/fftw.mk
+++ src/fftw.mk
@@ -3,7 +3,7 @@
 
 PKG             := fftw
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 11a8c31186ff5a7d686a79a3f21b2530888e0dc2
+$(PKG)_CHECKSUM := 11487180928d05746d431ebe7a176b52fe205cf9
 $(PKG)_SUBDIR   := fftw-$($(PKG)_VERSION)
 $(PKG)_FILE     := fftw-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.fftw.org/$($(PKG)_FILE)
