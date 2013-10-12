5d9f5b9c6216059b934e3734831946be35aecd0a
John Donoghue <john.donoghue@ieee.org>
2013-10-11 23:17:58 -0400

    Undo qt no-webkit changeset 3258:db4c94cfc651
    * src/qt.mk: readd -no-webkit configure option.

--- src/qt.mk
+++ src/qt.mk
@@ -120,6 +120,7 @@ define $(PKG)_BUILD
         -no-iconv \
         -opengl desktop \
         -openssl-linked \
+        -no-webkit \
         -no-phonon \
         -no-phonon-backend \
         -accessibility \
