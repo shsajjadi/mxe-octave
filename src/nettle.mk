e909d5b703bc6f432f9231438246f57b02341a49
John W. Eaton <jwe@octave.org>
2013-10-16 18:50:52 -0400

    update to new version of nettle

--- src/nettle.mk
+++ src/nettle.mk
@@ -3,7 +3,7 @@
 
 PKG             := nettle
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 1061754feb69dd01354525fa7eb6154b28ac887d
+$(PKG)_CHECKSUM := e17de3678b987841e88a724b7d2f6856d97ab139
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.lysator.liu.se/~nisse/archive/$($(PKG)_FILE)
@@ -47,15 +47,7 @@ define $(PKG)_BUILD
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         $(ENABLE_SHARED_OR_STATIC) \
         --prefix='$(HOST_PREFIX)'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' getopt.o getopt1.o
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     $(MAKE) -C '$(1)' -j 1 install
-
-    if [ -d $(HOST_PREFIX)/lib64 ]; then \
-      $(INSTALL) -d $(HOST_LIBDIR)/pkgconfig; \
-      mv $(HOST_PREFIX)/lib64/pkgconfig/* $(HOST_LIBDIR)/pkgconfig; \
-      rmdir $(HOST_PREFIX)/lib64/pkgconfig; \
-      mv $(HOST_PREFIX)/lib64/* $(HOST_LIBDIR); \
-    fi
 endef
 endif
