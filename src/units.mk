f62c28bd9f3f7316be35cba03600d4b4d0765876
John Donoghue <john.donoghue@ieee.org>
2013-10-21 14:39:17 -0400

    Add gnu units
    * src/units.mk: New file.
    * src/units-1-fixes.patch: New file.
    * index.html: Added units package information.
    * mk-dist: add units to make target.

---
+++ src/units.mk
@@ -0,0 +1,24 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := units
+$(PKG)_IGNORE   :=
+$(PKG)_CHECKSUM := e460371dc97034d17ce452e6b64991f7fd2d1409
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://ftp.gnu.org/gnu/$(PKG)/$($(PKG)_FILE)
+$(PKG)_DEPS     := 
+
+define $(PKG)_UPDATE
+    echo 'Warning: Updates are temporarily disabled for package $(PKG).' >&2;
+    echo $($(PKG)_VERSION)
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --prefix='$(HOST_PREFIX)' \
+        $(HOST_AND_BUILD_CONFIGURE_OPTIONS) 
+
+    $(MAKE) -C '$(1)' -j '$(JOBS)' 
+    $(MAKE) -C '$(1)' -j 1 install
+endef
