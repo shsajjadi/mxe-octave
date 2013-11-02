6e4ba126dec68e60361e27399dab6e41edf15ee6
John Donoghue <john.donoghue@ieee.org>
2013-11-02 12:31:57 -0400

    gl2ps: install nativemingw files and bin/dll.
    * src/gl2ps.mk: manually install libs and header in native mingw, copy .dll to the bin dir.

--- src/gl2ps.mk
+++ src/gl2ps.mk
@@ -27,5 +27,20 @@ define $(PKG)_BUILD
         $($(PKG)_CMAKE_FLAGS) \
         -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
         .
-    $(MAKE) -C '$(1)' -j '$(JOBS)' VERBOSE=1 install
+    $(MAKE) -C '$(1)' -j '$(JOBS)' VERBOSE=1 
+
+    # native mingw build doesnt want to install the files, even
+    # though it logs that it did
+    if [ x$(MXE_NATIVE_MINGW_BUILD) = xyes ]; then \
+      $(INSTALL) -m644 '$(1)/libgl2ps.a' '$(HOST_LIBDIR)'; \
+      $(INSTALL) -m644 '$(1)/libgl2ps.dll.a' '$(HOST_LIBDIR)'; \
+      $(INSTALL) -m644 '$(1)/libgl2ps.dll' '$(HOST_BINDIR)'; \
+      $(INSTALL) -m644 '$(1)/gl2ps.h' '$(HOST_INCDIR)'; \
+    else \
+      $(MAKE) -C '$(1)' -j 1 VERBOSE=1 install; \
+      if [ $(MXE_SYSTEM) = mingw ]; then \
+        echo "Install dll"; \
+        $(INSTALL) '$(1)/libgl2ps.dll' '$(HOST_BINDIR)/'; \
+      fi; \
+    fi
 endef
