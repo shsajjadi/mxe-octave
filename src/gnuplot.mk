0af0afcfc72bc2650d8ec77e3146482061e44e03
John Donoghue <john.donoghue@ieee.org>
2013-11-04 12:57:10 -0500

    Update gnuplot to 4.6.4
    * index,html: update gnuplot-version tag to 4.6.4.
    
    * src/gnuplot.mk: update checksum and build rules for 4.6.4.
    
    * src/gnuplot-1-fixes.patch: update patch for 4.6.4.

--- src/gnuplot.mk
+++ src/gnuplot.mk
@@ -3,10 +3,10 @@
 
 PKG             := gnuplot
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 1ea21a628223159b0297ae65fe8293afd5aab3c0
+$(PKG)_CHECKSUM := 54ee5ce9a0a2698b046064bd275e772673350013
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := gnuplot-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://sourceforge.net/projects/gnuplot/files/gnuplot/4.6.1/$($(PKG)_FILE)
+$(PKG)_URL      := http://sourceforge.net/projects/gnuplot/files/gnuplot/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     :=
 
 define $(PKG)_UPDATE
@@ -18,17 +18,16 @@ ifeq ($(MXE_SYSTEM),mingw)
 define $(PKG)_BUILD
     make -C '$(1)/config/mingw' CC='$(MXE_CC)' CXX='$(MXE_CXX)' RC='$(MXE_WINDRES)' -j '$(JOBS)' TARGET=gnuplot.exe gnuplot.exe
     make -C '$(1)/config/mingw' CC='$(MXE_CC)' CXX='$(MXE_CXX)' RC='$(MXE_WINDRES)' -j '$(JOBS)' TARGET=wgnuplot.exe wgnuplot.exe
-    make -C '$(1)/config/mingw' CC='$(MXE_CC)' CXX='$(MXE_CXX)' RC='$(MXE_WINDRES)' -j '$(JOBS)' wgnuplot.mnu
 
     $(INSTALL) -d '$(HOST_BINDIR)'
     $(INSTALL) -m755 '$(1)/config/mingw/gnuplot.exe' '$(HOST_BINDIR)'
     $(INSTALL) -m755 '$(1)/config/mingw/wgnuplot.exe' '$(HOST_BINDIR)'
-    $(INSTALL) -m644 '$(1)/config/mingw/wgnuplot.mnu' '$(HOST_BINDIR)'
+    $(INSTALL) -m644 '$(1)/src/win/wgnuplot.mnu' '$(HOST_BINDIR)'
 
     $(INSTALL) -d '$(TOP_DIR)/gnuplot/bin'
     $(INSTALL) -m755 '$(1)/config/mingw/gnuplot.exe' '$(TOP_DIR)/gnuplot/bin/'
     $(INSTALL) -m755 '$(1)/config/mingw/wgnuplot.exe' '$(TOP_DIR)/gnuplot/bin/'
-    $(INSTALL) -m644 '$(1)/config/mingw/wgnuplot.mnu' '$(TOP_DIR)/gnuplot/bin/'
+    $(INSTALL) -m644 '$(1)/src/win/wgnuplot.mnu' '$(TOP_DIR)/gnuplot/bin/'
 
 endef
 else
