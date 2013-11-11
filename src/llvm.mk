526dbfc93c3b880b8cf973c8f390f6af1e230d90
Michael Goffioul <michael.goffioul@gmail.com>
2013-11-11 11:03:06 -0500

    Support file listing for libffi and llvm.
    * src/libffi.mk: Use DESTDIR during install.
    * src/llvm.mk: Use DESTDIR during install. Create installation
    directory for build tools.

--- src/llvm.mk
+++ src/llvm.mk
@@ -26,19 +26,17 @@ define $(PKG)_BUILD
     cd '$(1)/.build' && cmake \
         -G "NMake Makefiles" \
         -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
-        -DLIBTYPE=STATIC \
+        -DBUILD_SHARED_LIBS=OFF \
         -DLLVM_TARGETS_TO_BUILD="X86" \
-        -DLLVM_ENABLE_FFI:BOOL=ON \
+        -DLLVM_ENABLE_FFI=ON \
         "-DFFI_INCLUDE_DIR=$(HOST_LIBDIR)/libffi-$(libffi_VERSION)/include" \
-        -DLLVM_REQUIRES_EH:BOOL=ON \
+        -DLLVM_REQUIRES_EH=ON \
         ../
     sed -i '/^	echo "/ {s/echo "/echo /;s/" >>/ >>/;}' \
         '$(1)/.build/tools/llvm-config/CMakeFiles/llvm-config.dir/build.make'
     cd '$(1)/.build' && \
-        env -u MAKE -u MAKEFLAGS \
-            LIB="`echo \`cd ../../../usr/i686-pc-mingw32/lib && pwd -W\` | sed -e 's,/,\\\\\\\\,g'`\;$$LIB" \
-            nmake && \
-        env -u MAKE -u MAKEFLAGS nmake install
+        env -u MAKE -u MAKEFLAGS nmake && \
+        env -u MAKE -u MAKEFLAGS nmake DESTDIR='$(3)' install
 endef
 else
 define $(PKG)_BUILD
@@ -51,19 +49,20 @@ define $(PKG)_BUILD
       $(ENABLE_SHARED_OR_STATIC) \
       --prefix='$(HOST_PREFIX)'
 
-    PATH='$(HOST_BINDIR):$(PATH)' $(MAKE) -C '$(1)/build' -j $(JOBS) install
+    PATH='$(HOST_BINDIR):$(PATH)' $(MAKE) -C '$(1)/build' -j $(JOBS) install DESTDIR='$(3)'
 
     # create import lib for the dll
     $(if $(filter yes, $(BUILD_SHARED)),
       cd '$(1)/build/tools/llvm-shlib/Release+Asserts' && \
         $(MXE_DLLTOOL) \
-         --dllname "LLVM-`$(HOST_BINDIR)/llvm-config --version`.dll" \
-         --def "LLVM-`$(HOST_BINDIR)/llvm-config --version`.def" \
-         --output-lib "libLLVM-`$(HOST_BINDIR)/llvm-config --version`.a"
+         --dllname "LLVM-`$(3)$(HOST_BINDIR)/llvm-config --version`.dll" \
+         --def "LLVM-`$(3)$(HOST_BINDIR)/llvm-config --version`.def" \
+         --output-lib "libLLVM-`$(3)$(HOST_BINDIR)/llvm-config --version`.a"
+      $(INSTALL) -d "$(3)$(HOST_LIBDIR)"
       cd '$(1)/build/tools/llvm-shlib/Release+Asserts' && \
         $(INSTALL) -m644 \
-         "libLLVM-`$(HOST_BINDIR)/llvm-config --version`.a" \
-         "$(HOST_LIBDIR)"
+         "libLLVM-`$(3)$(HOST_BINDIR)/llvm-config --version`.a" \
+         "$(3)$(HOST_LIBDIR)"
     )
 endef
 endif
@@ -79,9 +78,10 @@ define $(PKG)_BUILD
       --prefix='$(HOST_PREFIX)'
 
     $(MAKE) -C '$(1)/build' -j $(JOBS)
-    $(MAKE) -C '$(1)/build' -j 1 install
+    $(MAKE) -C '$(1)/build' -j 1 install DESTDIR='$(3)'
 
     if [ $(MXE_NATIVE_BUILD) = no ]; then \
+      $(INSTALL) -d '$(3)$(BUILD_TOOLS_PREFIX)/bin'; \
       $(INSTALL) -m755 '$(HOST_BINDIR)/llvm-config-host' '$(BUILD_TOOLS_PREFIX)/bin/llvm-config'; \
     fi
 
@@ -89,13 +89,14 @@ define $(PKG)_BUILD
     if [ $(MXE_SYSTEM) = mingw -a $(BUILD_SHARED) = yes ]; then \
       cd '$(1)/build/tools/llvm-shlib/Release+Asserts' && \
         $(MXE_DLLTOOL) \
-         --dllname "LLVM-`$(BUILD_TOOLS_PREFIX)/bin/llvm-config --version`.dll" \
-         --def "LLVM-`$(BUILD_TOOLS_PREFIX)/bin/llvm-config --version`.def" \
-         --output-lib "libLLVM-`$(BUILD_TOOLS_PREFIX)/bin/llvm-config --version`.a"; \
+         --dllname "LLVM-`$(3)$(BUILD_TOOLS_PREFIX)/bin/llvm-config --version`.dll" \
+         --def "LLVM-`$(3)$(BUILD_TOOLS_PREFIX)/bin/llvm-config --version`.def" \
+         --output-lib "libLLVM-`$(3)$(BUILD_TOOLS_PREFIX)/bin/llvm-config --version`.a"; \
+      $(INSTALL) -d "$(3)$(HOST_LIBDIR)"; \
       cd '$(1)/build/tools/llvm-shlib/Release+Asserts' && \
         $(INSTALL) -m644 \
-         "libLLVM-`$(BUILD_TOOLS_PREFIX)/bin/llvm-config --version`.a" \
-         "$(HOST_LIBDIR)"; \
+         "libLLVM-`$(3)$(BUILD_TOOLS_PREFIX)/bin/llvm-config --version`.a" \
+         "$(3)$(HOST_LIBDIR)"; \
     fi
 
 endef
