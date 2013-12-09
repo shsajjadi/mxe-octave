ed9457cf6aad69d2a0db494317a71ef1aadeb961
John Donoghue <john.donoghue@ieee.org>
2013-12-08 20:50:01 -0500

    Update optim version number to 1.2.2 in build_packages.m
    * build_packages.m: set optim version=1.2.2

--- build_packages.m
+++ build_packages.m
@@ -3,7 +3,7 @@ echo ("on");
 pkg install -global -auto general-1.3.2.tar.gz
 pkg install -global -auto miscellaneous-1.2.0.tar.gz
 pkg install -global -auto struct-1.0.10.tar.gz
-pkg install -global -auto optim-1.2.2a.tar.gz
+pkg install -global -auto optim-1.2.2.tar.gz
 pkg install -global -auto specfun-1.1.0.tar.gz
 pkg install -global -auto control-2.4.1.tar.gz
 pkg install -global -auto signal-1.2.0.tar.gz
