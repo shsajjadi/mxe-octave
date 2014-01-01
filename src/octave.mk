1c1dfce92d7c2f39b145d4c54e091c6840f31255
John Donoghue <john.donoghue@ieee.org>
2014-01-01 17:42:10 -0500

    Update stable-octave to 3.8.0
    * src/octave.mk: add creation of version file on octave install
    
    * src/stable-octave.mk: Merge changes of octave.mk into stable-octave.mk.
    
    * index.html: update stable-octave-version to 3.8.0.

--- src/octave.mk
+++ src/octave.mk
@@ -121,5 +121,8 @@ define $(PKG)_BUILD
     if [ $(MXE_SYSTEM) != msvc ]; then \
         $(MAKE) -C '$(1)/.build' -j '$(JOBS)' DESTDIR=$(TOP_DIR)/octave install; \
     fi
+
+    # create a file with latest installed octave rev in it
+    echo "$($(PKG)_VERSION)" > $(TOP_DIR)/octave/octave-version
 endef
 
