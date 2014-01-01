3f5cde5aa6d85c0fa5a60f09d6d428c1f1909c26
John Donoghue <john.donoghue@ieee.org>
2013-12-31 22:03:14 -0500

    of-control: update to 2.6.1
    * build_packages.m: Update install for control-2.6.1.tar.gz.
    
    * index.html: of-control-version = 2.6.1.
    
    * src/of-control.mk: update checksum for 2.6.1

--- build_packages.m
+++ build_packages.m
@@ -5,7 +5,7 @@ pkg install -global -auto miscellaneous-1.2.0.tar.gz
 pkg install -global -auto struct-1.0.10.tar.gz
 pkg install -global -auto optim-1.2.2.tar.gz
 pkg install -global -auto specfun-1.1.0.tar.gz
-pkg install -global -auto control-2.6.0.tar.gz
+pkg install -global -auto control-2.6.1.tar.gz
 pkg install -global -auto signal-1.2.2.tar.gz
 pkg install -global -auto communications-1.2.0.tar.gz
 pkg install -global -auto image-2.0.0.tar.gz
