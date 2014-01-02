21aad15263246bff2a1595fbac84e8309a37336a
John Donoghue <john.donoghue@ieee.org>
2014-01-01 19:06:32 -0500

    Add of-actuarial package
    * Makefile.am: add of-actuarial package to OCTAVE_FORGE_PACKAGES
    
    * build_packages.m: add load of of-actuarial-1.1.0.tar.gz
    
    * dist-files.mk: added of-actuarial package.
    
    * index.html: add of-actuarial-package.
    
    * src/of-actuarial.mk: New file.

--- build_packages.m
+++ build_packages.m
@@ -17,4 +17,5 @@ pkg install -global -auto odepkg-0.8.4.tar.gz
 pkg install -global -auto linear-algebra-2.2.0.tar.gz
 pkg install -global -auto sockets-1.0.8.tar.gz
 pkg install -global -auto zenity-0.5.7.tar.gz
+pkg install -global -auto actuarial-1.1.0.tar.gz
 
---
+++ src/of-actuarial.mk
@@ -0,0 +1,20 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := of-actuarial
+$(PKG)_IGNORE   :=
+$(PKG)_CHECKSUM := 89cc7a697f347329a107676afcf09575faa2f386
+$(PKG)_REMOTE_SUBDIR := 
+$(PKG)_SUBDIR   := actuarial
+$(PKG)_FILE     := actuarial-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/octave/$($(PKG)_FILE)?download
+$(PKG)_DEPS     := 
+
+define $(PKG)_UPDATE
+    echo 'Warning: Updates are temporarily disabled for package $(PKG).' >&2;
+    echo $($(PKG)_VERSION)
+endef
+
+define $(PKG)_BUILD
+    $(OCTAVE_FORGE_PKG_BUILD)
+endef
