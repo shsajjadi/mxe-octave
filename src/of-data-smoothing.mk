993166589b07bc07b0aff99dcba3f8c60a0ceed7
John Donoghue <john.donoghue@ieee.org>
2014-01-03 14:05:33 -0500

    Add octave forge data-smoothing package
    * Makefile.am: add data-smoothing to OCTAVE_FORGE_PACKAGES.
    
    * build_packages.m: dd data-smoothing package to install.
    
    * dist-files.mk: add of-data-smoothing.mk.
    
    * index.html: add of-data-smoothing-package.
    
    * src/data-smoothing.mk: New file.

---
+++ src/of-data-smoothing.mk
@@ -0,0 +1,20 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := of-data-smoothing
+$(PKG)_IGNORE   :=
+$(PKG)_CHECKSUM := 28fb1233f0db586bccac9317f0f0eff84bb9842e
+$(PKG)_REMOTE_SUBDIR := 
+$(PKG)_SUBDIR   := data-smoothing
+$(PKG)_FILE     := data-smoothing-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/octave/$($(PKG)_FILE)?download
+$(PKG)_DEPS     := optim
+
+define $(PKG)_UPDATE
+    echo 'Warning: Updates are temporarily disabled for package $(PKG).' >&2;
+    echo $($(PKG)_VERSION)
+endef
+
+define $(PKG)_BUILD
+    $(OCTAVE_FORGE_PKG_BUILD)
+endef
