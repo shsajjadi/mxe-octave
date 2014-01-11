dcc810abbb4c244ed147ac16c0af9ae2c756a68a
John Donoghue <john.donoghue@ieee.org>
2014-01-11 10:51:36 -0500

    of-io: update to 2.0.2
    * build_packages.m: update to io-2.0.2.tar.gz
    
    * index.html: update of-io-version to 2.0.2.
    
    * src/of-io.mk: update checksum.

--- src/of-io.mk
+++ src/of-io.mk
@@ -3,7 +3,7 @@
 
 PKG             := of-io
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 3744a01b45cb8519ba1a5477ab1ce7a16ead889f
+$(PKG)_CHECKSUM := 6dcb18186678ebf89936c9ea9e94737d6bf2d3cc
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := io
 $(PKG)_FILE     := io-$($(PKG)_VERSION).tar.gz
