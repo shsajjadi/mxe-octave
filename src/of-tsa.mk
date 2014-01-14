a3eadc15c4838acc7063917dc48f1225d1559a9d
John Donoghue <john.donoghue@ieee.org>
2014-01-13 21:28:53 -0500

    Add octave forge tsa package
    * Makefile.in: Add of-tsa.
    
    * build_packages.m: Add install of tsa-4.2.7.tar.gz
    
    * dist-files: Add of-tsa.mk.
    
    * index.html: Added of-tsa-package.
    
    * src/of-tsa.mk: New file.
    
    * src/libgomp.mk: Install to HOST_PREFIX

---
+++ src/of-tsa.mk
@@ -0,0 +1,20 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := of-tsa
+$(PKG)_IGNORE   :=
+$(PKG)_CHECKSUM := 80d2faa61c32043bf1edc2c0fa48fe0c00eacd89
+$(PKG)_REMOTE_SUBDIR := 
+$(PKG)_SUBDIR   := tsa
+$(PKG)_FILE     := tsa-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/octave/$($(PKG)_FILE)?download
+$(PKG)_DEPS     := libgomp
+
+define $(PKG)_UPDATE
+    echo 'Warning: Updates are temporarily disabled for package $(PKG).' >&2;
+    echo $($(PKG)_VERSION)
+endef
+
+define $(PKG)_BUILD
+    $(OCTAVE_FORGE_PKG_BUILD)
+endef
