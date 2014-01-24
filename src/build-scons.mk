f7e7dadbe80b0973415e05da2dcf4483916a67a7
John W. Eaton <jwe@octave.org>
2014-01-24 12:36:20 -0500

    add build-scon.mk file omitted from previous commit

---
+++ src/build-scons.mk
@@ -0,0 +1,19 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := build-scons
+$(PKG)_IGNORE   :=
+$(PKG)_CHECKSUM := 728edf20047a9f8a537107dbff8d8f803fd2d5e3
+$(PKG)_SUBDIR   := scons-$($(PKG)_VERSION)
+$(PKG)_FILE     := scons-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://prdownloads.sourceforge.net/scons/$($(PKG)_FILE)
+$(PKG)_DEPS     := build-python
+
+define $(PKG)_UPDATE
+    echo 'Warning: Updates are temporarily disabled for package $(PKG).' >&2;
+    echo $($(PKG)_VERSION)
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && python setup.py install
+endef
