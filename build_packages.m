d1fb86dcf7e3682ce9b1208cc7d67bf4237ae177
John Donoghue <john.donoghue@ieee.org>
2014-01-28 18:23:35 -0500

    of-statistics: Update of-statistics to 1.2.3
    * build_packages.m: update install of statistics to 1.2.3.
    
    * index.html: update of-statistics-version to 1.2.3.
    
    * src/of-statistics.mk: update checksum.

--- build_packages.m
+++ build_packages.m
@@ -16,7 +16,7 @@ pkg install signal-1.2.2.tar.gz
 pkg install communications-1.2.0.tar.gz
 pkg install image-2.2.0.tar.gz
 pkg install io-2.0.2.tar.gz
-pkg install statistics-1.1.3.tar.gz
+pkg install statistics-1.2.3.tar.gz
 pkg install geometry-1.7.0.tar.gz
 pkg install windows-1.2.1.tar.gz
 pkg install odepkg-0.8.4.tar.gz
