c2bcdaba3c0f986abe84297d4a7e746be66726a4
John Donoghue <john.donoghue@ieee.org>
2014-02-01 14:17:59 -0500

    Update curl to 7.35.0
    * src/curl.mk: update to version 7.35.0.

--- src/curl.mk
+++ src/curl.mk
@@ -3,8 +3,8 @@
 
 PKG             := curl
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 7.33.0
-$(PKG)_CHECKSUM := 00c4293c336a1f987cf93c9ff385c5eb865db5d0
+$(PKG)_VERSION  := 7.35.0
+$(PKG)_CHECKSUM := ba7b8866308e3dc1264aac42342a2094cac3cace
 $(PKG)_SUBDIR   := curl-$($(PKG)_VERSION)
 $(PKG)_FILE     := curl-$($(PKG)_VERSION).tar.lzma
 $(PKG)_URL      := http://curl.haxx.se/download/$($(PKG)_FILE)
