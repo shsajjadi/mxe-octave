b120cf19cc0580810a7b76aa31d10e3d61877df2
John Donoghue <john.donoghue@ieee.org>
2014-02-02 09:16:38 -0500

    Update lapack to 3.5.0
    * src/lapack.mk: update to 3.5.0.

--- src/lapack.mk
+++ src/lapack.mk
@@ -2,8 +2,8 @@
 # See index.html for further information.
 
 PKG             := lapack
-$(PKG)_VERSION  := 3.4.2
-$(PKG)_CHECKSUM := 93a6e4e6639aaf00571d53a580ddc415416e868b
+$(PKG)_VERSION  := 3.5.0
+$(PKG)_CHECKSUM := 5870081889bf5d15fd977993daab29cf3c5ea970
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tgz
 $(PKG)_URL      := http://www.netlib.org/$(PKG)/$($(PKG)_FILE)
