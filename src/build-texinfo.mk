b147d195e72a800873864dfe96cb0ece1faf2312
John W. Eaton <jwe@octave.org>
2014-02-03 18:11:00 -0500

    back down to texinfo 4.3

--- src/build-texinfo.mk
+++ src/build-texinfo.mk
@@ -3,10 +3,10 @@
 
 PKG             := build-texinfo
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.2
-$(PKG)_CHECKSUM := fbbc35c5857d11d1164c8445c78b66ad6d472072
-$(PKG)_SUBDIR   := texinfo-$($(PKG)_VERSION)
-$(PKG)_FILE     := texinfo-$($(PKG)_VERSION).tar.xz
+$(PKG)_VERSION  := 4.13a
+$(PKG)_CHECKSUM := a1533cf8e03ea4fa6c443b73f4c85e4da04dead0
+$(PKG)_SUBDIR   := texinfo-4.13
+$(PKG)_FILE     := texinfo-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := ftp://ftp.gnu.org/gnu/texinfo/$($(PKG)_FILE)
 $(PKG)_DEPS     := 
 
@@ -20,6 +20,13 @@ define $(PKG)_BUILD
     cd    '$(1).build' && '$(1)/configure' \
         --prefix='$(BUILD_TOOLS_PREFIX)'
 
-    $(MAKE) -C '$(1).build' -j '$(JOBS)'
-    $(MAKE) -C '$(1).build' -j 1 install DESTDIR='$(3)'
+    $(MAKE) -C '$(1).build/gnulib/lib' -j '$(JOBS)'
+    $(MAKE) -C '$(1).build/lib' -j '$(JOBS)'
+    $(MAKE) -C '$(1).build/makeinfo' -j '$(JOBS)'
+    $(MAKE) -C '$(1).build/util' -j '$(JOBS)'
+
+    $(MAKE) -C '$(1).build/gnulib/lib' -j 1 install DESTDIR='$(3)'
+    $(MAKE) -C '$(1).build/lib' -j 1 install DESTDIR='$(3)'
+    $(MAKE) -C '$(1).build/makeinfo' -j 1 install DESTDIR='$(3)'
+    $(MAKE) -C '$(1).build/util' -j 1 install DESTDIR='$(3)'
 endef
