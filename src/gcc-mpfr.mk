e0a0a038f493eb06d6b584bf1d93cde4e28ed0c6
John W. Eaton <jwe@octave.org>
2014-02-04 15:25:39 -0500

    update gcc dependencies

--- src/gcc-mpfr.mk
+++ src/gcc-mpfr.mk
@@ -3,14 +3,24 @@
 
 PKG             := gcc-mpfr
 $(PKG)_IGNORE    = $(mpfr_IGNORE)
-$(PKG)_VERSION  := 3.1.1
+$(PKG)_VERSION   = $(mpfr_VERSION)
 $(PKG)_CHECKSUM  = $(mpfr_CHECKSUM)
 $(PKG)_SUBDIR    = $(mpfr_SUBDIR)
 $(PKG)_FILE      = $(mpfr_FILE)
 $(PKG)_URL       = $(mpfr_URL)
 $(PKG)_URL_2     = $(mpfr_URL_2)
-$(PKG)_DEPS     :=
+$(PKG)_DEPS     := gcc-gmp
 
 define $(PKG)_UPDATE
     echo $(mpfr_VERSION)
+endef
+
+define $(PKG)_BUILD
+    mkdir '$(1).build'
+    cd    '$(1).build' && '$(1)/configure' \
+        --prefix='$(BUILD_TOOLS_PREFIX)' \
+        --disable-shared \
+        --with-gmp='$(BUILD_TOOLS_PREFIX)'
+    $(MAKE) -C '$(1).build' -j '$(JOBS)'
+    $(MAKE) -C '$(1).build' -j 1 install
 endef
