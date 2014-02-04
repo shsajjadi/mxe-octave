d3d88cf96cb3c73f4d5197e1d204e6ee4b3ee9a0
John Donoghue <john.donoghue@ieee.org>
2014-02-04 06:18:45 -0500

    gcc: fix gmp-prefix
    * src/gcc-isl.mk: set gmp-prefix from PREFIX to BUILD_TOOLS_PREFIX

--- src/gcc-isl.mk
+++ src/gcc-isl.mk
@@ -20,7 +20,7 @@ define $(PKG)_BUILD
     cd    '$(1).build' && '$(1)/configure' \
         --prefix='$(BUILD_TOOLS_PREFIX)' \
         $(ENABLE_SHARED_OR_STATIC) \
-        --with-gmp-prefix='$(PREFIX)'
+        --with-gmp-prefix='$(BUILD_TOOLS_PREFIX)'
     $(MAKE) -C '$(1).build' -j '$(JOBS)'
     $(MAKE) -C '$(1).build' -j 1 install
 endef
