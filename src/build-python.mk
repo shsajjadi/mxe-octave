28c3ace6577b994f3c03041b387522c9b1062ece
John Donoghue <john.donoghue@ieee.org>
2014-02-12 18:23:02 -0500

    build-python: no depeancy on build-gcc if using system gcc.
    * src/build-python.mk: only add build-gcc as a dependancy if not using system gcc.

--- src/build-python.mk
+++ src/build-python.mk
@@ -8,7 +8,10 @@ $(PKG)_CHECKSUM := 8328d9f1d55574a287df384f4931a3942f03da64
 $(PKG)_SUBDIR   := Python-$($(PKG)_VERSION)
 $(PKG)_FILE     := Python-$($(PKG)_VERSION).tgz
 $(PKG)_URL      := http://www.python.org/ftp/python/2.7.6/$($(PKG)_FILE)
-$(PKG)_DEPS     := build-gcc
+$(PKG)_DEPS     := 
+ifneq ($(USE_SYSTEM_GCC),yes)
+    $(PKG)_DEPS     += build-gcc
+endif
 
 define $(PKG)_UPDATE
     echo 'Warning: Updates are temporarily disabled for package $(PKG).' >&2;
