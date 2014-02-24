e31f44c9907f4cac4ce34645b6dff38ec85cb81d
John Donoghue <john.donoghue@ieee.org>
2014-02-23 20:31:49 -0500

    build_packages.m: update for general-1.3.4
    * build_packages.m: update general to 1.3.4.

--- build_packages.m
+++ build_packages.m
@@ -6,7 +6,7 @@ if length(packagedir) > 0
   cd(packagedir);
 endif
 % install the packages
-pkg install general-1.3.2.tar.gz
+pkg install general-1.3.4.tar.gz
 pkg install miscellaneous-1.2.0.tar.gz
 pkg install struct-1.0.10.tar.gz
 pkg install optim-1.2.2.tar.gz
