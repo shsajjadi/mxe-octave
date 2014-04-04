a1af4be34c7bea74e551c506749335b2609e3088
John W. Eaton <jwe@octave.org>
2014-04-04 11:37:53 -0400

    new patch for stable-octave package

--- dist-files.mk
+++ dist-files.mk
@@ -546,6 +546,7 @@ SRC_FILES_1 := \
   stable-octave-1-docinstall.patch \
   stable-octave-1-fixes.patch \
   stable-octave-1-xerror.patch \
+  stable-octave-2-xerror.patch \
   stable-octave.mk \
   suitesparse-1.patch \
   suitesparse.mk \
