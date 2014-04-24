8d1362b39acc6ee6e6265202a0c16d7b2e4faf0c
John Donoghue <John Donoghue@localhost>
2014-04-24 15:44:00 -0400

    texinfo: update to install info.exe
    * src/mingw-texinfo-1-fixes.patch: new file.
    
    * dist-files.mk: add mingw-texinfo-1-fixes.patch.
    
    * src/texinfo.mk: build libs and build and install info.exe

--- src/texinfo.mk
+++ src/texinfo.mk
@@ -10,6 +10,11 @@ $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := ftp://ftp.gnu.org/gnu/texinfo/$($(PKG)_FILE)
 $(PKG)_DEPS     := # libgnurx
 
+ifeq ($(MXE_SYSTEM),mingw)
+    $(PKG)_DEPS += pcre
+    $(PKG)_LIBS += LIBS='-lpcre -lpcreposix'
+endif
+
 define $(PKG)_UPDATE
     echo 'Warning: Updates are temporarily disabled for package texinfo.' >&2;
     echo $(texinfo_VERSION)
@@ -20,12 +25,19 @@ define $(PKG)_BUILD
     cd '$(1).build' && '$(1)/configure' \
         $(CONFIGURE_CPPFLAGS) $(CONFIGURE_LDFLAGS) \
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
-        --prefix='$(HOST_PREFIX)'
+        --prefix='$(HOST_PREFIX)' $($(PKG)_LIBS)
 
-    ## All we need for Octave is makeinfo.
+    ## All we need for Octave is makeinfo
     $(MAKE) -C '$(1).build/lib' -j '$(JOBS)'
     $(MAKE) -C '$(1).build/gnulib/lib' -j '$(JOBS)'
     $(MAKE) -C '$(1).build/makeinfo' -j '$(JOBS)'
- 
     $(MAKE) -C '$(1).build/makeinfo' -j 1 install DESTDIR='$(3)'
+
+    # octave-cli needs info to display help
+    # need build native tools in order to build info
+    $(MAKE) -C '$(1).build/tools/lib' -j $(JOBS) 
+    $(MAKE) -C '$(1).build/tools/gnulib/lib' -j $(JOBS) 
+    $(MAKE) -C '$(1).build/tools/info' -j $(JOBS) makedoc
+    $(MAKE) -C '$(1).build/info' -j '$(JOBS)' funs.h ginfo.exe
+    $(INSTALL) '$(1).build/info/ginfo.exe' '$(3)$(HOST_BINDIR)/info.exe'
 endef
