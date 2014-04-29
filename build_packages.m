35aa3a98064de183b49851e2c79f43f8ed9b34f8
John Donoghue <john.donoghue@ieee.org>
2014-04-29 15:34:27 -0400

    of-*: update of-control=2.6.3, of-io=2.2.1, io-image=2.2.1
    * src/build_packages.m: update version numbers of packages.
    
    * src/of-control.mk: update version, checksum.
    
    * src/of-image.mk: update version, checksum.
    
    * src/of-io.mk: update version, checksum.

--- build_packages.m
+++ build_packages.m
@@ -11,11 +11,11 @@ pkg install miscellaneous-1.2.0.tar.gz
 pkg install struct-1.0.10.tar.gz
 pkg install optim-1.2.2.tar.gz
 pkg install specfun-1.1.0.tar.gz
-pkg install control-2.6.2.tar.gz
+pkg install control-2.6.3.tar.gz
 pkg install signal-1.3.0.tar.gz
 pkg install communications-1.2.0.tar.gz
-pkg install image-2.2.0.tar.gz
-pkg install io-2.0.2.tar.gz
+pkg install image-2.2.1.tar.gz
+pkg install io-2.2.1.tar.gz
 pkg install statistics-1.2.3.tar.gz
 pkg install geometry-1.7.0.tar.gz
 pkg install windows-1.2.1.tar.gz
