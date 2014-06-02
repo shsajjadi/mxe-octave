d3e7f425e2c1ac3b8cea22ef3ad0ceba7abba579
John Donoghue <John Donoghue@localhost>
2014-06-02 15:58:44 -0400

    build-python: Update python to 2.7.7
    * src/build-python.mk: update version 2.7.7.

--- src/build-python.mk
+++ src/build-python.mk
@@ -3,11 +3,11 @@
 
 PKG             := build-python
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.7.6
-$(PKG)_CHECKSUM := 8328d9f1d55574a287df384f4931a3942f03da64
+$(PKG)_VERSION  := 2.7.7
+$(PKG)_CHECKSUM := 1db01d7f325d8ceaf986976800106018b82ae45a
 $(PKG)_SUBDIR   := Python-$($(PKG)_VERSION)
 $(PKG)_FILE     := Python-$($(PKG)_VERSION).tgz
-$(PKG)_URL      := http://www.python.org/ftp/python/2.7.6/$($(PKG)_FILE)
+$(PKG)_URL      := http://www.python.org/ftp/python/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := 
 ifneq ($(USE_SYSTEM_GCC),yes)
     $(PKG)_DEPS     += build-gcc
