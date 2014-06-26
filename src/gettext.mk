af982ddd6ab65fbab4112f0ce2838cdd7c87eeb8
John Donoghue <John Donoghue@localhost>
2014-06-26 08:07:53 -0400

    gettext: update to 0.19.1
    * src/gettext.mk: update to version 0.19.1

--- src/gettext.mk
+++ src/gettext.mk
@@ -3,8 +3,8 @@
 
 PKG             := gettext
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.18.3.1
-$(PKG)_CHECKSUM := a32c19a6e39450748f6e56d2ac6b8b0966a5ab05
+$(PKG)_VERSION  := 0.19.1
+$(PKG)_CHECKSUM := 94cd6e81976aeb8ba35cf73967c60b72dd04af8d
 $(PKG)_SUBDIR   := gettext-$($(PKG)_VERSION)
 $(PKG)_FILE     := gettext-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/gettext/$($(PKG)_FILE)
