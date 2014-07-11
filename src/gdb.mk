57b90d54bc05a8f0acd30827656f20e0637dba48
John W. Eaton <jwe@octave.org>
2014-07-11 15:57:24 -0400

    update gdb package

--- src/gdb.mk
+++ src/gdb.mk
@@ -2,8 +2,8 @@
 # See index.html for further information.
 
 PKG             := gdb
-$(PKG)_VERSION  := 7.5
-$(PKG)_CHECKSUM := 79b61152813e5730fa670c89e5fc3c04b670b02c
+$(PKG)_VERSION  := 7.7.1
+$(PKG)_CHECKSUM := 35228319f7c715074a80be42fff64c7645227a80
 $(PKG)_SUBDIR   := gdb-$($(PKG)_VERSION)
 $(PKG)_FILE     := gdb-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/$(PKG)/$($(PKG)_FILE)
