dcc73adb87e297ef678315850432e7840ff0be75
John Donoghue <John Donoghue@localhost>
2014-09-16 09:16:06 -0400

    of-io: update to 2.2.4
    * src/of-io.mk: update version/checksum
    
    * build_packages.m: update to io-2.2.4

--- build_packages.m
+++ build_packages.m
@@ -15,7 +15,7 @@ pkg install control-2.6.5.tar.gz
 pkg install signal-1.3.0.tar.gz
 pkg install communications-1.2.0.tar.gz
 pkg install image-2.2.1.tar.gz
-pkg install io-2.2.2.tar.gz
+pkg install io-2.2.4.tar.gz
 pkg install statistics-1.2.3.tar.gz
 pkg install geometry-1.7.0.tar.gz
 pkg install windows-1.2.1.tar.gz
