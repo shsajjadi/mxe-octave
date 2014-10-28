6d813a2a487832f448935433d40559d5defea0fd
John Donoghue <John Donoghue@localhost>
2014-10-28 07:57:11 -0400

    build_packages.m: add helper function to install packages
    * build_packages.m: add helper function to catch errors when installing packages.

--- build_packages.m
+++ build_packages.m
@@ -5,30 +5,45 @@ echo ("on");
 if length(packagedir) > 0
   cd(packagedir);
 endif
+
+% helper function to try install a package, and recover
+function try_install (pkgname)
+  currdir = pwd ();
+ 
+  try
+    pkg ('install', pkgname, '-noauto')
+  catch err
+    warning (err.identifier, err.message);
+  end_try_catch
+
+  cd (currdir);
+endfunction
+
 % install the packages
-pkg install general-1.3.4.tar.gz
-pkg install miscellaneous-1.2.1.tar.gz
-pkg install struct-1.0.10.tar.gz
-pkg install optim-1.4.0.tar.gz
-pkg install specfun-1.1.0.tar.gz
-pkg install control-2.6.6.tar.gz
-pkg install signal-1.3.0.tar.gz
-pkg install communications-1.2.0.tar.gz
-pkg install image-2.2.2.tar.gz
-pkg install io-2.2.4.tar.gz
-pkg install statistics-1.2.3.tar.gz
-pkg install geometry-1.7.0.tar.gz
-pkg install windows-1.2.1.tar.gz
-pkg install odepkg-0.8.4.tar.gz
-pkg install linear-algebra-2.2.0.tar.gz
-pkg install sockets-1.0.8.tar.gz
-pkg install zenity-0.5.7.tar.gz
-pkg install actuarial-1.1.0.tar.gz
-pkg install data-smoothing-1.3.0.tar.gz
-pkg install fuzzy-logic-toolkit-0.4.5.tar.gz
-pkg install quaternion-2.2.2.tar.gz
-pkg install fits-1.0.5.tar.gz
-pkg install fl-core-1.0.0.tar.gz
-pkg install tsa-4.2.7.tar.gz
-pkg install dicom-0.1.1.tar.gz
-pkg install netcdf-1.0.5.tar.gz
+try_install general-1.3.4.tar.gz
+try_install miscellaneous-1.2.1.tar.gz
+try_install struct-1.0.10.tar.gz
+try_install optim-1.4.0.tar.gz
+try_install specfun-1.1.0.tar.gz
+try_install control-2.6.6.tar.gz
+try_install signal-1.3.0.tar.gz
+try_install communications-1.2.0.tar.gz
+try_install image-2.2.2.tar.gz
+try_install io-2.2.4.tar.gz
+try_install statistics-1.2.3.tar.gz
+try_install geometry-1.7.0.tar.gz
+try_install windows-1.2.1.tar.gz
+try_install odepkg-0.8.4.tar.gz
+try_install linear-algebra-2.2.0.tar.gz
+try_install sockets-1.0.8.tar.gz
+try_install zenity-0.5.7.tar.gz
+try_install actuarial-1.1.0.tar.gz
+try_install data-smoothing-1.3.0.tar.gz
+try_install fuzzy-logic-toolkit-0.4.5.tar.gz
+try_install quaternion-2.2.2.tar.gz
+try_install fits-1.0.5.tar.gz
+try_install fl-core-1.0.0.tar.gz
+try_install tsa-4.2.7.tar.gz
+try_install dicom-0.1.1.tar.gz
+try_install netcdf-1.0.5.tar.gz
+
