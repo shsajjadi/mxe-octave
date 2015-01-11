e65a5ec5c9bb40f62ef7ccbaacf15d1343d3ae57
John Donoghue <john.donoghue@ieee.org>
2015-01-10 20:13:01 -0500

    uuid: use mingw-w64 instead of w32
    * src/uuid.mk: use mingw-w64, make shared lib using the installed libuuid.a

--- src/uuid.mk
+++ src/uuid.mk
@@ -2,12 +2,12 @@
 # See index.html for further information.
 
 PKG             := uuid
-$(PKG)_IGNORE   = $(w32api_IGNORE)
+$(PKG)_IGNORE   = $(mingw-w64_IGNORE)
 $(PKG)_VERSION  := 3.17
-$(PKG)_CHECKSUM = $(w32api_CHECKSUM)
-$(PKG)_SUBDIR   = $(w32api_SUBDIR)
-$(PKG)_FILE     = $(w32api_FILE)
-$(PKG)_URL      = $(w32api_URL)
+$(PKG)_CHECKSUM = $(mingw-w64_CHECKSUM)
+$(PKG)_SUBDIR   = $(mingw-w64_SUBDIR)
+$(PKG)_FILE     = $(mingw-w64_FILE)
+$(PKG)_URL      = $(mingw-w64_URL)
 $(PKG)_DEPS     =
 
 define $(PKG)_UPDATE
@@ -17,7 +17,6 @@ endef
 define $(PKG)_BUILD
     if [ $(BUILD_SHARED) = yes ]; then \
       $(INSTALL) -d '$(HOST_BINDIR)'; \
-      $(MAKE_SHARED_FROM_STATIC) --ar '$(MXE_AR)' --ld '$(MXE_CC)' '$(1)/lib/libuuid.a' --install '$(INSTALL)' --libdir '$(HOST_LIBDIR)' --bindir '$(HOST_BINDIR)'; \
-      rm -f '$(HOST_LIBDIR)/libuuid.dll.a'; \
+      $(MAKE_SHARED_FROM_STATIC) --ar '$(MXE_AR)' --ld '$(MXE_CC)' '$(HOST_LIBDIR)/libuuid.a' --install '$(INSTALL)' --libdir '$(HOST_LIBDIR)' --bindir '$(HOST_BINDIR)'; \
     fi
 endef
