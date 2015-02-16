3b60c6255010b4b5631eff32ee177896cb7c7a58
John Donoghue <John Donoghue@localhost>
2015-02-16 14:06:56 -0500

    dbus: update to 1.9.10
    * src/dbus.mk: update version, checksum

--- src/dbus.mk
+++ src/dbus.mk
@@ -3,8 +3,8 @@
 
 PKG             := dbus
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.8.4
-$(PKG)_CHECKSUM := 316f1196312a88cc858ba810d4e5d16f70ab9d58
+$(PKG)_VERSION  := 1.9.10
+$(PKG)_CHECKSUM := a9741b6d017d6374c20bac2ea41262683b3e2386
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://$(PKG).freedesktop.org/releases/$(PKG)/$($(PKG)_FILE)
