f5377c9887c911582ec12e10589d314f1807da3b
John Donoghue <John Donoghue@localhost>
2015-03-03 14:03:53 -0500

    plotutils: add rule to compile for gnulinux
    * src/plotutils.mk: add PKG_BUILD for non windows targets

--- src/plotutils.mk
+++ src/plotutils.mk
@@ -11,6 +11,7 @@ $(PKG)_URL      := http://ftpmirror.gnu.org/$(PKG)/$($(PKG)_FILE)
 $(PKG)_URL_2    := http://ftp.gnu.org/gnu/$(PKG)/$($(PKG)_FILE)
 $(PKG)_DEPS     := libpng pthreads
 
+
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://ftp.gnu.org/gnu/plotutils/?C=M;O=D' | \
     grep '<a href="plotutils-' | \
@@ -18,7 +19,8 @@ define $(PKG)_UPDATE
     head -1
 endef
 
-define $(PKG)_BUILD
+ifeq ($(MXE_WINDOWS_BUILD),yes)
+  define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         --prefix='$(HOST_PREFIX)' \
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
@@ -40,4 +42,21 @@ define $(PKG)_BUILD
     else \
        $(MAKE) -C '$(1)' -j 1 bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS= man_MANS= INFO_DEPS= install; \
     fi
-endef
+  endef
+else
+  define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --prefix='$(HOST_PREFIX)' \
+        $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
+        $(CONFIGURE_CPPFLAGS) $(CONFIGURE_LDFLAGS) \
+        $(ENABLE_SHARED_OR_STATIC) \
+        --enable-libplotter \
+        --enable-libxmi \
+        --with-png \
+        --without-x \
+        CFLAGS='-DNO_SYSTEM_GAMMA' LIBS='-lpng -lz'
+
+    $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS= man_MANS= INFO_DEPS=
+    $(MAKE) -C '$(1)' -j 1 bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS= man_MANS= INFO_DEPS= install
+  endef
+endif
