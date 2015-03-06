5abc312a138ffa7cdba7e983dc2af070617bf2a0
Avinoam <Avinoam@localhost>
2015-03-06 08:19:56 +0200

    default-octave: update for 4.0.0-rc1
    * src/default-octave.mk: update version to 4.0.0-rc1

--- src/default-octave.mk
+++ src/default-octave.mk
@@ -6,7 +6,7 @@
 
 PKG             := default-octave
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.9.1+
+$(PKG)_VERSION  := 4.0.0-rc1
 $(PKG)_CHECKSUM := ## No checksum
 $(PKG)_SUBDIR   := octave-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.gz
