03b90a999f7377dd812909426a340ac150a3763e
John W. Eaton <jwe@octave.org>
2015-04-10 12:11:50 -0400

    octave.mk: update for 4.0.0-rc3

--- src/octave.mk
+++ src/octave.mk
@@ -3,8 +3,8 @@
 
 PKG             := octave
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.0.0-rc2
-$(PKG)_CHECKSUM := 7301cf36625dce769d649d53ff7a125943920b0f
+$(PKG)_VERSION  := 4.0.0-rc3
+$(PKG)_CHECKSUM := 003ad4bfeca86b2f0e749f3e70f5c224735c7bf6
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := ftp://alpha.gnu.org/gnu/octave/$($(PKG)_FILE)
