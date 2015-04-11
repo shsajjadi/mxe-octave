4a0dd9b1b1189ca6d113a7206a21a34540684097
John Donoghue <john.donoghue@ieee.org>
2015-04-11 11:48:50 -0400

    default-octave: make octave version match mercurial tag 4.0.0-rc3
    * src/default-octave.mk: make octave version match mercurial tag 4.0.0-rc3

--- src/default-octave.mk
+++ src/default-octave.mk
@@ -6,7 +6,7 @@
 
 PKG             := default-octave
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.0.0-rc2
+$(PKG)_VERSION  := 4.0.0-rc3
 $(PKG)_CHECKSUM := ## No checksum
 $(PKG)_SUBDIR   := octave-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.gz
