f2150388f265efaf4324ae868419152f98168628
John Donoghue <john.donoghue@ieee.org>
2015-04-13 06:40:23 -0400

    Add nurbs package
    * src/of-nurbs.mk: new file
    
    * src/of-nurbs-1-fixes.patch: new file
    
    * Makefile.in: add nurbs
    
    * index.html: add nurbs package
    
    * dist-files.mk: add of-nurbs.mk, of-nurbs-1-fixes.patch

---
+++ src/of-nurbs.mk
@@ -0,0 +1,22 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := of-nurbs
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.3.9
+$(PKG)_CHECKSUM := 4727f2e38486bdc8452d7657094fbdb7a3980ba5
+$(PKG)_REMOTE_SUBDIR := 
+$(PKG)_SUBDIR   := nurbs
+$(PKG)_FILE     := nurbs-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := '$(OCTAVE_FORGE_BASE_URL)/$($(PKG)_FILE)/download'
+$(PKG)_DEPS     := 
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://$(SOURCEFORGE_MIRROR)/projects/octave/files/Octave%20Forge%20Packages/Individual%20Package%20Releases/' | \
+    $(SED) -n 's,.*title="nurbs-\([0-9][^"]*\).tar.gz".*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    $(OCTAVE_FORGE_PKG_BUILD)
+endef
