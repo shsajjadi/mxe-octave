d96af8d94f40a44bdc4af47c7a47720c06c3038f
John Donoghue <John Donoghue@localhost>
2015-04-14 13:16:40 -0400

    of-octcdf: add to mxe
    * src/of-octcdf.mk: new file
    
    * src/of-octcdf-1-cross-fixes.patch: new file
    
    * dist-files.mk: add of-octcdf.mk, of-octcdf-1-cross-fixes.patch
    
    * Makefile.in: add octcdf to forge packages
    
    * index.html: add of-octcdf

---
+++ src/of-octcdf.mk
@@ -0,0 +1,22 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := of-octcdf
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.1.8
+$(PKG)_CHECKSUM := 0550e4d5741d5e720d6ad8a42edc8c1a451f07e5
+$(PKG)_REMOTE_SUBDIR := 
+$(PKG)_SUBDIR   := octcdf
+$(PKG)_FILE     := octcdf-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := '$(OCTAVE_FORGE_BASE_URL)/$($(PKG)_FILE)/download'
+$(PKG)_DEPS     := netcdf
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://$(SOURCEFORGE_MIRROR)/projects/octave/files/Octave%20Forge%20Packages/Individual%20Package%20Releases/' | \
+    $(SED) -n 's,.*title="octcdf-\([0-9][^"]*\).tar.gz".*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    $(OCTAVE_FORGE_PKG_BUILD)
+endef
