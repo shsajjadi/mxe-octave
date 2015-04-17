d5981ae2cb6da8da7ceebef2acfc1f2b95612cfe
John Donoghue <John Donoghue@localhost>
2015-04-17 08:24:07 -0400

    of-stk: add to mxe
    * src/of-stk.mk: new file
    
    * dist-files.mk: add of-stk.mk
    
    * index.html: add stk
    
    * Makefile.in: add stk to forge packages
    
    * build_packages.m: add stk

---
+++ src/of-stk.mk
@@ -0,0 +1,22 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := of-stk
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.2.1
+$(PKG)_CHECKSUM := 8b2f47ecb4e468782b2b3622b3ddbb82f56673b5
+$(PKG)_REMOTE_SUBDIR := 
+$(PKG)_SUBDIR   := stk
+$(PKG)_FILE     := stk-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := '$(OCTAVE_FORGE_BASE_URL)/$($(PKG)_FILE)/download'
+$(PKG)_DEPS     := 
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://$(SOURCEFORGE_MIRROR)/projects/octave/files/Octave%20Forge%20Packages/Individual%20Package%20Releases/' | \
+    $(SED) -n 's,.*title="stk-\([0-9][^"]*\).tar.gz".*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    $(OCTAVE_FORGE_PKG_BUILD)
+endef
