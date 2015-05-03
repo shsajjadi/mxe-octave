ad41756f9d1fb3da4150b0f8a91f7917644e10b6
John Donoghue <john.donoghue@ieee.org>
2015-05-03 15:04:50 -0400

    of-generate_html: update to v0.1.8
    * src/of-generate_html.mk: update version/checksum
    
    * build_packages.m: update generate_html version

--- build_packages.m
+++ build_packages.m
@@ -48,7 +48,7 @@ try_install netcdf-1.0.6.tar.gz
 try_install ltfat-2.0.1.tar.gz
 try_install database-2.3.1.tar.gz
 try_install instrument-control-0.2.1.tar.gz
-try_install generate_html-0.1.7.tar.gz
+try_install generate_html-0.1.8.tar.gz
 try_install financial-0.4.0.tar.gz
 try_install stk-2.2.1.tar.gz
 try_install splines-1.2.7.tar.gz
