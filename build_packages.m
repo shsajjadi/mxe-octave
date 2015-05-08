d62dbd16244059de269c950d21e036752480e748
John Donoghue <John Donoghue@localhost>
2015-05-08 11:00:50 -0400

    of-splines: update to 1.2.8
    * src/of-splines.mk: update version, checksum
    
    * build_packages.m: update splines version

--- build_packages.m
+++ build_packages.m
@@ -51,7 +51,7 @@ try_install instrument-control-0.2.1.tar.gz
 try_install generate_html-0.1.8.tar.gz
 try_install financial-0.4.0.tar.gz
 try_install stk-2.2.1.tar.gz
-try_install splines-1.2.7.tar.gz
+try_install splines-1.2.8.tar.gz
 try_install dataframe-1.1.0.tar.gz
 try_install lssa-0.1.2.tar.gz
 try_install queueing-1.2.3.tar.gz
--- src/of-splines.mk
+++ src/of-splines.mk
@@ -3,10 +3,10 @@
 
 PKG             := of-splines
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.2.7
-$(PKG)_CHECKSUM := d5a28ff0a6d7e42efc9756de78296bbf5d9ede22
+$(PKG)_VERSION  := 1.2.8
+$(PKG)_CHECKSUM := 9b4824aa26b8b49c46fe6937e46a01a25451dcc5
 $(PKG)_REMOTE_SUBDIR := 
-$(PKG)_SUBDIR   := splines
+$(PKG)_SUBDIR   := splines-$($(PKG)_VERSION)
 $(PKG)_FILE     := splines-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := '$(OCTAVE_FORGE_BASE_URL)/$($(PKG)_FILE)/download'
 $(PKG)_DEPS     := 
