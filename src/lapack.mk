38939097ef9f0649d7f44c8b5e46861013d5f8e7
John Donoghue <John Donoghue@localhost>
2015-06-16 12:05:52 -0400

    lapack: combine native mingw, msvc rules
    * src/lapack.mk: remove native mingw rule an use msvc rule

--- src/lapack.mk
+++ src/lapack.mk
@@ -27,7 +27,6 @@ define $(PKG)_UPDATE
 endef
 
 ifeq ($(MXE_NATIVE_MINGW_BUILD),yes)
-ifeq ($(MXE_SYSTEM),msvc)
 define $(PKG)_BUILD
     cd '$(1)' && \
         cp INSTALL/make.inc.gfortran make.inc && \
@@ -39,40 +38,16 @@ define $(PKG)_BUILD
     $(MAKE) -C '$(1)' -j '$(JOBS)' VERBOSE=1 lapacklib
 
     if [ $(BUILD_SHARED) = yes ]; then \
-        $(MAKE_SHARED_FROM_STATIC) --ar '$(MXE_AR)' --ld '$(MXE_F77)' '$(1)/liblapack.a' --install '$(INSTALL)' --libdir '$(3)$(HOST_LIBDIR)' --bindir '$(3)$(HOST_BINDIR)' -lblas; \
+        $(MAKE_SHARED_FROM_STATIC) --ar '$(MXE_AR)' --ld '$(MXE_F77)' '$(1)/liblapack.a' --install '$(INSTALL)' --libdir '$(HOST_LIBDIR)' --bindir '$(HOST_BINDIR)' -lblas; \
     fi
 
-    $(INSTALL) -d '$(3)$(HOST_LIBDIR)/pkgconfig' 
+    $(INSTALL) -d '$(HOST_LIBDIR)/pkgconfig' 
     $(SED) -e 's/@LAPACK_VERSION@/$($(PKG)_VERSION)/' \
            -e 's,@prefix@,$(HOST_PREFIX),' \
 	   -e 's,@libdir@,$${prefix}/lib,' '$(1)/lapack.pc.in' > '$(1)/lapack.pc'
-    $(INSTALL) '$(1)/lapack.pc' '$(3)$(HOST_LIBDIR)/pkgconfig/'
+    $(INSTALL) '$(1)/lapack.pc' '$(HOST_LIBDIR)/pkgconfig/'
 
 endef
-else
-define $(PKG)_BUILD
-    cd '$(1)' && cmake \
-        -G 'MSYS Makefiles' \
-        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
-        -DCMAKE_Fortran_FLAGS='$($(PKG)_DEFAULT_INTEGER_8_FLAG)' \
-        $($(PKG)_BLAS_CONFIG_OPTS) \
-        .
-    $(MAKE) -C '$(1)/SRC' -j '$(JOBS)' VERBOSE=1 install DESTDIR='$(3)'
-
-    if [ $(BUILD_SHARED) = yes ]; then \
-      $(INSTALL) '$(1)/lib/liblapack.dll.a' '$(3)$(HOST_LIBDIR)/'; \
-      $(INSTALL) '$(1)/lib/liblapack.lib' '$(3)$(HOST_LIBDIR)/'; \
-      $(INSTALL) '$(1)/bin/liblapack.dll' '$(3)$(HOST_BINDIR)/'; \
-    fi
-    if [ $(BUILD_STATIC) = yes ]; then \
-      $(INSTALL) '$(1)/lib/liblapack.a' '$(3)$(HOST_LIBDIR)/'; \
-    fi
-
-    $(INSTALL) -d '$(3)$(HOST_LIBDIR)/pkgconfig' 
-    $(INSTALL) '$(1)/lapack.pc' '$(3)$(HOST_LIBDIR)/pkgconfig/'
-
-endef
-endif
 else
 define $(PKG)_BUILD
     cd '$(1)' && cmake \
