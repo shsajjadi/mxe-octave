cdb9bb6d467e042ed99fd71fde76187df3716e43
John D <John D@localhost>
2015-07-11 19:48:48 -0400

    src-mysy-bash: update for available package in sourceforge
    * src/src-msys-bash.mk: update for http://sourceforge.net/projects/mingw/files/MSYS/Base/bash/bash-3.1.17-4/bash-3.1.17-4-msys-1.0.16-src.tar.lzma/download

--- src/src-msys-bash.mk
+++ src/src-msys-bash.mk
@@ -4,13 +4,12 @@
 PKG             := src-msys-bash
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 3.1.17-4
-$(PKG)_CHECKSUM := 
+$(PKG)_CHECKSUM := 6ec21594c64d7e95081fb77d19c687a08857c885
 $(PKG)_REMOTE_SUBDIR := bash/bash-$($(PKG)_VERSION)
 $(PKG)_SUBDIR   := 
-$(PKG)_FILE     := bash-$($(PKG)_VERSION)-msys-1.0.13-src.tar.lzma
+$(PKG)_FILE     := bash-$($(PKG)_VERSION)-msys-1.0.16-src.tar.lzma
 $(PKG)_URL      := $(MSYS_BASE_URL)/$($(PKG)_REMOTE_SUBDIR)/$($(PKG)_FILE)/download
 $(PKG)_DEPS     := 
-
 define $(PKG)_UPDATE
     $(WGET) -q -O- '$(MSYS_BASE_URL)/bash' | \
     $(SED) -n 's,.*title="bash-\([0-9][^"]*\)".*,\1,p' | \
