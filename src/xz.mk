872f28c83e8e5849d2343d02c5b30fc6a0bcdead
John Donoghue <John Donoghue@localhost>
2015-10-01 13:13:27 -0400

    xz: update to 5.2.2
    * src/xz.mk: update version, checksum
    
    * src/build-xz.mk: update version, checksum

--- src/xz.mk
+++ src/xz.mk
@@ -3,8 +3,8 @@
 
 PKG             := xz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.2.1
-$(PKG)_CHECKSUM := 6022493efb777ff4e872b63a60be1f1e146f3c0b
+$(PKG)_VERSION  := 5.2.2
+$(PKG)_CHECKSUM := 14663612422ab61386673be78fbb2556f50a1f08
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://tukaani.org/xz/$($(PKG)_FILE)
@@ -23,5 +23,5 @@ define $(PKG)_BUILD
         $(ENABLE_SHARED_OR_STATIC) \
         --disable-threads \
         --disable-nls
-    $(MAKE) -C '$(1)'/src/liblzma -j '$(JOBS)' install
+    $(MAKE) -C '$(1)'/src/liblzma -j '$(JOBS)' install DESTDIR='$(3)'
 endef
