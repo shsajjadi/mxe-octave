e91557b964ace446aa0aa6623ab3178f1a403054
John Donoghue <John Donoghue@localhost>
2015-11-30 16:48:14 -0500

    of-splines: update to 1.2.9
    * src/of-splines.mk: update version, checksum, subdir
    
    * build_packages.m: update splines version

--- src/of-splines.mk
+++ src/of-splines.mk
@@ -3,10 +3,10 @@
 
 PKG             := of-splines
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.2.8
-$(PKG)_CHECKSUM := 9b4824aa26b8b49c46fe6937e46a01a25451dcc5
+$(PKG)_VERSION  := 1.2.9
+$(PKG)_CHECKSUM := 35c845a158ab5601319baf937031c3eb995b9bbe
 $(PKG)_REMOTE_SUBDIR := 
-$(PKG)_SUBDIR   := splines-$($(PKG)_VERSION)
+$(PKG)_SUBDIR   := splines
 $(PKG)_FILE     := splines-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := '$(OCTAVE_FORGE_BASE_URL)/$($(PKG)_FILE)/download'
 $(PKG)_DEPS     := 
