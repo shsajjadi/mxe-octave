c92cb615ece8ab1ad1697e60b3280ea21cd77091
John Donoghue <John Donoghue@localhost>
2015-12-11 13:18:42 -0500

    texinfo: update to v6.0 (Bug #46637)
    * Makefile.in: include msys-perl, msys-libcrypt  when installing mingw
    
    * src/build-texinfo.mk: update version, checksum, build sequence
    
    * src/texinfo.mk: update version, checksum, build sequence
    
    * src/mingw-texinfo-1-fixes.patch: update patch
    
    * src/build-texinfo-1-fixes.patch: removed
    
    * src/texinfo-1-fixes.patch: removed
    
    * dist-files.mk: removed ref to build-texinfo-1-fixes.patch, texinfo-1-fixes.patch

--- src/build-texinfo.mk
+++ src/build-texinfo.mk
@@ -3,9 +3,9 @@
 
 PKG             := build-texinfo
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.13a
-$(PKG)_CHECKSUM := a1533cf8e03ea4fa6c443b73f4c85e4da04dead0
-$(PKG)_SUBDIR   := texinfo-4.13
+$(PKG)_VERSION  := 6.0
+$(PKG)_CHECKSUM := 110d45256c4219c88dc2fdb8c9c1a20749e4e7c5
+$(PKG)_SUBDIR   := texinfo-$($(PKG)_VERSION)
 $(PKG)_FILE     := texinfo-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := ftp://ftp.gnu.org/gnu/texinfo/$($(PKG)_FILE)
 $(PKG)_DEPS     := 
@@ -21,12 +21,10 @@ define $(PKG)_BUILD
         --prefix='$(BUILD_TOOLS_PREFIX)'
 
     $(MAKE) -C '$(1).build/gnulib/lib' -j '$(JOBS)'
-    $(MAKE) -C '$(1).build/lib' -j '$(JOBS)'
-    $(MAKE) -C '$(1).build/makeinfo' -j '$(JOBS)'
     $(MAKE) -C '$(1).build/util' -j '$(JOBS)'
+    $(MAKE) -C '$(1).build/tp' -j '$(JOBS)'
 
     $(MAKE) -C '$(1).build/gnulib/lib' -j 1 install DESTDIR='$(3)'
-    $(MAKE) -C '$(1).build/lib' -j 1 install DESTDIR='$(3)'
-    $(MAKE) -C '$(1).build/makeinfo' -j 1 install DESTDIR='$(3)'
     $(MAKE) -C '$(1).build/util' -j 1 install DESTDIR='$(3)'
+    $(MAKE) -C '$(1).build/tp' -j 1 install DESTDIR='$(3)'
 endef
