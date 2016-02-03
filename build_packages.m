cf39a346b4c197109fc0597e04eb5e35d6aa788a
John Donoghue <John Donoghue@localhost>
2016-02-03 08:42:37 -0500

    of-instrument-control: update to 0.2.2
    * src/of-instrument-control.mk: update version, checksum, update directory name
    
    * build_packages.m: update nstrument-control version

--- build_packages.m
+++ build_packages.m
@@ -47,7 +47,7 @@ try_install dicom-0.1.1.tar.gz
 try_install netcdf-1.0.8.tar.gz
 try_install ltfat-2.1.1.tar.gz
 try_install database-2.3.2.tar.gz
-try_install instrument-control-0.2.1.tar.gz
+try_install instrument-control-0.2.2.tar.gz
 try_install generate_html-0.1.9.tar.gz
 try_install financial-0.5.0.tar.gz
 try_install stk-2.3.4.tar.gz
