42162114c5766e7f74e4a1bb66d1ed845eead38a
John Donoghue <John Donoghue@localhost>
2016-03-15 08:03:52 -0400

    of-netcdf: do not package of-octcdf in installer
    * src/of-octcdf.mk: add depreciated comment in package
    
    * build_packages.m: remove octcdf from build_packages script
    
    * Makefile.in: remove octcdf from installed of- list

--- build_packages.m
+++ build_packages.m
@@ -56,7 +56,6 @@ try_install dataframe-1.1.0.tar.gz
 try_install lssa-0.1.2.tar.gz
 try_install queueing-1.2.3.tar.gz
 try_install nurbs-1.3.10.tar.gz
-try_install octcdf-1.1.8.tar.gz
 try_install strings-1.2.0.tar.gz
 try_install ga-0.10.0.tar.gz
 try_install interval-1.4.1.tar.gz
