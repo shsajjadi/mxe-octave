358f3203e96be797d3aad80b1a39fc1dd4cf7f78
John Donoghue <John Donoghue@localhost>
2016-03-25 15:26:45 -0400

    build_packages.m: update database version to installed 2.4.1

--- build_packages.m
+++ build_packages.m
@@ -46,7 +46,7 @@ try_install tsa-4.4.1.tar.gz
 try_install dicom-0.1.1.tar.gz
 try_install netcdf-1.0.9.tar.gz
 try_install ltfat-2.1.2.tar.gz
-try_install database-2.3.2.tar.gz
+try_install database-2.4.1.tar.gz
 try_install instrument-control-0.2.2.tar.gz
 try_install generate_html-0.1.10.tar.gz
 try_install financial-0.5.0.tar.gz
