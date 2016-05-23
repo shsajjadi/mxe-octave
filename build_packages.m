3fe10e8ca973b0f081a564a8ea554c263ddeb80b
John Donoghue <John Donoghue@localhost>
2016-05-23 12:54:26 -0400

    of-splines: update to 1.3.0
    * src/of-splines.mk: update version, checksum
    
    * build_packages.m: update splines version

--- build_packages.m
+++ build_packages.m
@@ -51,7 +51,7 @@ try_install instrument-control-0.2.2.tar.gz
 try_install generate_html-0.1.10.tar.gz
 try_install financial-0.5.0.tar.gz
 try_install stk-2.3.4.tar.gz
-try_install splines-1.2.9.tar.gz
+try_install splines-1.3.0.tar.gz
 try_install dataframe-1.1.0.tar.gz
 try_install lssa-0.1.2.tar.gz
 try_install queueing-1.2.3.tar.gz
