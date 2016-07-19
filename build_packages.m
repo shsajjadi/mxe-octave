03d0da252ca0ab2b1e71e32cad9cecbd0591c168
John Donoghue <John Donoghue@localhost>
2016-07-19 16:45:05 -0400

    of-generate_html: update to 0.1.12
    * src/of-generate_html.mk: update version, checksum
    
    * build_packages.m: update generate_html version.

--- build_packages.m
+++ build_packages.m
@@ -55,7 +55,7 @@ unwind_protect
   try_install ltfat-2.1.2.tar.gz
   try_install database-2.4.1.tar.gz
   try_install instrument-control-0.2.2.tar.gz
-  try_install generate_html-0.1.11.tar.gz
+  try_install generate_html-0.1.12.tar.gz
   try_install financial-0.5.0.tar.gz
   try_install stk-2.3.4.tar.gz
   try_install splines-1.3.1.tar.gz
