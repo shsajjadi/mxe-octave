f4a403db97d461f1b47d9a95543c7d0fab30b0f3
John W. Eaton <jwe@octave.org>
2016-10-02 02:14:21 -0400

    default-octave.mk: Update pkg version to 4.3.0+.

--- src/default-octave.mk
+++ src/default-octave.mk
@@ -6,7 +6,7 @@
 
 PKG             := default-octave
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.2.0-rc2
+$(PKG)_VERSION  := 4.3.0+
 $(PKG)_CHECKSUM := ## No checksum
 $(PKG)_SUBDIR   := octave-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.gz
