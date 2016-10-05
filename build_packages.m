2f07ab1d39122060dd2c9facd60acc619bc9ba3d
John D <John D@localhost>
2016-10-05 15:39:04 -0400

    of-struct: update to 1.0.14
    * src/of-struct.mk: update version, checksum
    
    * build_packages.m: update struct version

--- build_packages.m
+++ build_packages.m
@@ -30,7 +30,7 @@ unwind_protect
   % install the packages
   try_install general-2.0.0.tar.gz
   try_install miscellaneous-1.2.1.tar.gz
-  try_install struct-1.0.13.tar.gz
+  try_install struct-1.0.14.tar.gz
   try_install optim-1.5.1.tar.gz
   try_install specfun-1.1.0.tar.gz
   try_install control-3.0.0.tar.gz
