acd723e2d0f94ece1746b76e5e56739ed4b98331
John D <John D@localhost>
2016-10-13 07:57:25 -0400

    Add qt5 to path in cmdshell
    * installer-files/cmdshell.bat: add qt5\bin to path

--- installer-files/cmdshell.bat
+++ installer-files/cmdshell.bat
@@ -5,9 +5,10 @@ set OCTAVE_HOME=%~dp0
 Rem   Set up PATH. Make sure the octave bin dir
 Rem   comes first.
 
-set PATH=%OCTAVE_HOME%bin;%PATH%
+set PATH=%OCTAVE_HOME%qt5\bin;%OCTAVE_HOME%bin;%PATH%
 set TERM=cygwin
 set GS=gs.exe
+set GNUTERM=windows
 
 %OCTAVE_HOME%\bin\bash.exe
 
