9300ccf9ddce2b67ef05322dca9a1884190f5e40
John Donoghue <john.donoghue@ieee.org>
2016-11-13 09:59:42 -0500

    * build_packages.m: set io package version to 2.4.5

--- build_packages.m
+++ build_packages.m
@@ -37,7 +37,7 @@ unwind_protect
   try_install signal-1.3.2.tar.gz
   try_install communications-1.2.1.tar.gz
   try_install image-2.6.1.tar.gz
-  try_install io-2.4.3.tar.gz
+  try_install io-2.4.5.tar.gz
   try_install statistics-1.3.0.tar.gz
   try_install geometry-2.1.1.tar.gz
   try_install windows-1.2.4.tar.gz
