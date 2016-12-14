f566290becc52d932a0b3f66dab79d6d76d3d2f0
John D <John D@localhost>
2016-12-14 08:10:07 -0500

    * src/libidn.mk: update to v1.33

--- src/libidn.mk
+++ src/libidn.mk
@@ -3,8 +3,8 @@
 
 PKG             := libidn
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.31
-$(PKG)_CHECKSUM := 0bb34003a0fe05a91e60d346803401f16c82a1fb
+$(PKG)_VERSION  := 1.33
+$(PKG)_CHECKSUM := 57872fdc665dcc585e16f4ac0bb35374b1103f7e
 $(PKG)_SUBDIR   := libidn-$($(PKG)_VERSION)
 $(PKG)_FILE     := libidn-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := ftp://ftp.gnu.org/gnu/libidn/$($(PKG)_FILE)
