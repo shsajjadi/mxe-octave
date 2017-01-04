545bfe21ba40c15a1dd12a3ecec65185afd57270
John D <John D@localhost>
2017-01-04 08:32:26 -0500

    msys-msys-core: update to 1.0.19
    * src/msys-msys-core.mk: update version, checksum
    
    * src/src-msys-msys-core.mk: update version, checksum

--- src/msys-msys-core.mk
+++ src/msys-msys-core.mk
@@ -3,11 +3,11 @@
 
 PKG             := msys-msys-core
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.18-1
-$(PKG)_CHECKSUM := 36d52ca7066eb6ad0da68c6f31214416f4c9dcec
+$(PKG)_VERSION  := 1.0.19-1
+$(PKG)_CHECKSUM := 9200450ad3df8c83be323c9b14ae344d5c1ca784
 $(PKG)_REMOTE_SUBDIR := msys-core/msys-$($(PKG)_VERSION)
 $(PKG)_SUBDIR   := 
-$(PKG)_FILE     := msysCORE-$($(PKG)_VERSION)-msys-1.0.18-bin.tar.lzma
+$(PKG)_FILE     := msysCORE-$($(PKG)_VERSION)-msys-1.0.19-bin.tar.xz
 $(PKG)_URL      := $(MSYS_BASE_URL)/$($(PKG)_REMOTE_SUBDIR)/$($(PKG)_FILE)/download
 $(PKG)_DEPS     := 
 
