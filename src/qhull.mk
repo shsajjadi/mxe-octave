e7a5abac0b60a08baca7707cc79e91f9aeb87912
John D <John D@localhost>
2017-01-17 07:32:34 -0500

    qhull: dont install docs
    * src/qhull.mk: add MXE_DISABLE_DOCS in make install

--- src/qhull.mk
+++ src/qhull.mk
@@ -28,5 +28,5 @@ define $(PKG)_BUILD
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         $(ENABLE_SHARED_OR_STATIC) \
         --prefix='$(HOST_PREFIX)' && $(CONFIGURE_POST_HOOK)
-    $(MAKE) -C '$(1)/.build' -j '$(JOBS)' install DESTDIR='$(3)'
+    $(MAKE) -C '$(1)/.build' -j '$(JOBS)' install DESTDIR='$(3)' $(MXE_DISABLE_DOCS)
 endef
