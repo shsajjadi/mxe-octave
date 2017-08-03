6355add2e96fed54644da215bbe33facbfa3b0d4
John D <John D@localhost>
2017-08-03 10:33:05 -0400

    libidn2: update to 2.0.3
    * dist-files.mk: remove ref to patch file
    
    * src/libidn2.mk: update version, checksum, disable docs
    
    * src/libidn2-1-fixes.patch: deleted

--- src/libidn2.mk
+++ src/libidn2.mk
@@ -3,11 +3,11 @@
 
 PKG             := libidn2
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.16
-$(PKG)_CHECKSUM := 26311b538897a8ed0569922132f2139ee3ec6a28
+$(PKG)_VERSION  := 2.0.3
+$(PKG)_CHECKSUM := 9264dfafbad5979a9b38d8092e9f3fe6481843ea
 $(PKG)_SUBDIR   := libidn2-$($(PKG)_VERSION)
 $(PKG)_FILE     := libidn2-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := ftp://alpha.gnu.org/gnu/libidn/$($(PKG)_FILE)
+$(PKG)_URL      := ftp://ftp.gnu.org/gnu/libidn/$($(PKG)_FILE)
 $(PKG)_DEPS     := gettext libiconv libunistring
 
 define $(PKG)_UPDATE
@@ -22,6 +22,7 @@ define $(PKG)_BUILD
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         $(ENABLE_SHARED_OR_STATIC) \
         --prefix='$(HOST_PREFIX)' \
+        --disable-doc \
         && $(CONFIGURE_POST_HOOK)
     $(MAKE) -C '$(1)' -j '$(JOBS)' $(MXE_DISABLE_PROGS) DESTDIR='$(3)'
     $(MAKE) -C '$(1)' -j 1 install $(MXE_DISABLE_PROGS) $(MXE_DISABLE_DOCS) DESTDIR='$(3)'
