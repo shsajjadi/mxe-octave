8152927643e1ae253d7d62f0dccbcfe9e3603638
John D <John D@localhost>
2017-09-20 01:14:09 -0400

    expat: update to v2.2.4
    * src/expat.mk: update version, checksum, use MXE_DISABLE_PROGS

--- src/expat.mk
+++ src/expat.mk
@@ -3,8 +3,8 @@
 
 PKG             := expat
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.2.3
-$(PKG)_CHECKSUM := e2be3a2aa8d12ce70184aab418d0d5d96ed86d0e
+$(PKG)_VERSION  := 2.2.4
+$(PKG)_CHECKSUM := 3394d6390c041a8f5dec1d5fe7c4af0a23ae4504
 $(PKG)_SUBDIR   := expat-$($(PKG)_VERSION)
 $(PKG)_FILE     := expat-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/expat/expat/$($(PKG)_VERSION)/$($(PKG)_FILE)
@@ -22,5 +22,5 @@ define $(PKG)_BUILD
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         $(ENABLE_SHARED_OR_STATIC) \
         --prefix='$(HOST_PREFIX)' && $(CONFIGURE_POST_HOOK)
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS= DESTDIR='$(3)'
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install $(MXE_DISABLE_PROGS) DESTDIR='$(3)'
 endef
