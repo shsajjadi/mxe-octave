eed58624c2fad0b9aa3e00e0a4a1b99a53663951
John W. Eaton <jwe@octave.org>
2017-10-06 08:27:03 -0400

    glu: New module.

---
+++ src/glu.mk
@@ -0,0 +1,27 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := glu
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 9.0.0
+$(PKG)_CHECKSUM := c2814bbaf1e60e28a75ec80f4646047c0da742bd
+$(PKG)_SUBDIR   := glu-$($(PKG)_VERSION)
+$(PKG)_FILE     := glu-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := ftp://ftp.freedesktop.org/pub/mesa/glu/$($(PKG)_FILE)
+$(PKG)_DEPS     := mesa
+
+ifeq ($(MXE_WINDOWS_BUILD),yes)
+  define $(PKG)_BUILD
+  endef
+else
+  define $(PKG)_BUILD
+    mkdir '$(1)/.build'
+    cd '$(1)/.build' && $($(PKG)_CONFIGURE_ENV) '$(1)/configure' \
+        $(CONFIGURE_CPPFLAGS) $(CONFIGURE_LDFLAGS) \
+        $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
+        --prefix='$(HOST_PREFIX)' \
+        && $(CONFIGURE_POST_HOOK)
+
+    $(MAKE) -C '$(1)/.build' -j '$(JOBS)' install DESTDIR='$(3)'
+  endef
+endif
