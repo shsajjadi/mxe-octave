7637a5b342638c93dd10fadb9970cd47c0e6fdca
John W. Eaton <jwe@octave.org>
2017-10-18 09:27:04 -0400

    s2tc: enable Windows build.

--- src/s2tc.mk
+++ src/s2tc.mk
@@ -10,19 +10,21 @@ $(PKG)_FILE     := s2tc-$($(PKG)_VERSION).zip
 $(PKG)_URL      := https://github.com/divVerent/s2tc/archive/v$($(PKG)_VERSION).zip
 $(PKG)_DEPS     := 
 
-ifeq ($(MXE_WINDOWS_BUILD),yes)
-  define $(PKG)_BUILD
-  endef
-else
-  define $(PKG)_BUILD
-    cd '$(1)' && ./autogen.sh
-    mkdir '$(1)/.build'
-    cd '$(1)/.build' && $($(PKG)_CONFIGURE_ENV) '$(1)/configure' \
-        $(CONFIGURE_CPPFLAGS) $(CONFIGURE_LDFLAGS) \
-        $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
-        --prefix='$(HOST_PREFIX)' \
-        && $(CONFIGURE_POST_HOOK)
+define $(PKG)_BUILD
+  cd '$(1)' && ./autogen.sh
+  mkdir '$(1)/.build'
+  cd '$(1)/.build' && $($(PKG)_CONFIGURE_ENV) '$(1)/configure' \
+      $(CONFIGURE_CPPFLAGS) $(CONFIGURE_LDFLAGS) \
+      $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
+      $(ENABLE_SHARED_OR_STATIC) \
+      --prefix='$(HOST_PREFIX)' \
+      --disable-tools \
+      && $(CONFIGURE_POST_HOOK)
 
-    $(MAKE) -C '$(1)/.build' -j '$(JOBS)' install DESTDIR='$(3)'
-  endef
-endif
+  $(MAKE) -C '$(1)/.build' -j '$(JOBS)' install DESTDIR='$(3)'
+
+  ## Mesa attempts to dynamically load dxtn.dll on Windows systems.
+  if [ $(MXE_WINDOWS_BUILD) = yes ]; then \
+    mv '$(3)$(HOST_BINDIR)/libtxc_dxtn-0.dll' '$(3)$(HOST_BINDIR)/dxtn.dll'; \
+  fi
+endef
