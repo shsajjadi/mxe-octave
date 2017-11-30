8d12bf4b3468452b4fb676f6f539e89917adebb6
John D <John D@localhost>
2017-11-30 13:41:24 -0500

    zeromq: update to v4.2.2
    * src/zeromq.mk: update version/checksum, implement PKG_UPDATE

--- src/zeromq.mk
+++ src/zeromq.mk
@@ -3,16 +3,17 @@
 
 PKG             := zeromq
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.2.1
-$(PKG)_CHECKSUM := 2145aef176dbc944822a9b4585273d17e9b9fb7b
+$(PKG)_VERSION  := 4.2.2
+$(PKG)_CHECKSUM := 5ef9b1c385f4b897e6a9e4847e5355758c20de76
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://github.com/$(PKG)/libzmq/releases/download/v$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := pthreads
 
 define $(PKG)_UPDATE
-    echo 'Warning: Updates are temporarily disabled for package $(PKG).' >&2;
-    echo $($(PKG)_VERSION)
+    $(WGET) -q -O- 'https://github.com/zeromq/libzmq/tags' | \
+    $(SED) -n 's|.*releases/tag/v\([^"]*\).*|\1|p' | $(SORT) -V | \
+    tail -1
 endef
 
 define $(PKG)_BUILD
