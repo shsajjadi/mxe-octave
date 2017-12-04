23c595ce96a974e87ea5485678a7909c49335e1f
John D <John D@localhost>
2017-12-04 16:47:54 -0500

    lcms: update to v2.9
    * src/lcms.mk: update version, checksum
    
    * src/lcms-1-fixes.patch: updated patch

--- src/lcms.mk
+++ src/lcms.mk
@@ -3,8 +3,8 @@
 
 PKG             := lcms
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.8
-$(PKG)_CHECKSUM := e9535ec4a572b8fc7a1c405c35e6f4dc97714197
+$(PKG)_VERSION  := 2.9
+$(PKG)_CHECKSUM := 60bea9875e017dd1c466e988c2ad98f8766e4e55
 $(PKG)_SUBDIR   := $(PKG)$(word 1,$(subst ., ,$($(PKG)_VERSION)))-$(subst a,,$($(PKG)_VERSION))
 $(PKG)_FILE     := $(PKG)$(word 1,$(subst ., ,$($(PKG)_VERSION)))-$(subst a,,$($(PKG)_VERSION)).tar.gz
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$(subst a,,$($(PKG)_VERSION))/$($(PKG)_FILE)
