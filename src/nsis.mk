71062a9715db5932bf8af2eb67edf26db7965696
John W. Eaton <jwe@octave.org>
2018-02-26 10:34:17 -0500

    nsis: update to version 3.03

--- src/nsis.mk
+++ src/nsis.mk
@@ -3,42 +3,33 @@
 
 PKG             := nsis
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.50
-$(PKG)_CHECKSUM := 3748b81b83fb3d717da0db4178c228ac6e2a0543
+$(PKG)_VERSION  := 3.03
+$(PKG)_CHECKSUM := 101a9dd2dc06e8239d30741e0d723b633e091a2f
 $(PKG)_SUBDIR   := nsis-$($(PKG)_VERSION)-src
 $(PKG)_FILE     := nsis-$($(PKG)_VERSION)-src.tar.bz2
-$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/nsis/NSIS 2/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/nsis/NSIS 3/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     :=
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://sourceforge.net/p/nsis/code/HEAD/tree/NSIS/tags/' | \
-    grep 'title="v' | \
-    $(SED) -n 's,.*href="v\([0-9]\)\([^"]*\)".*,\1.\2,p' | \
+    $(WGET) -q -O- 'https://nsis.sourceforge.io/Download' | \
+    $(SED) -n 's,.*nsis-\([0-9.]\+\)-src.tar.*,\1,p' | \
     tail -1
 endef
-ifeq ($(ENABLE_WINDOWS_64),yes)
-define $(PKG)_BUILD
-    cd '$(1)' && patch -p1 < $(TOP_DIR)/src/win64-nsis.patch
 
-    cd '$(1)' && scons VERBOSE=1 \
-        PATH='$(PATH)' \
-        MINGW_CROSS_PREFIX='$(MXE_TOOL_PREFIX)' \
-        PREFIX='$(BUILD_TOOLS_PREFIX)' \
-        APPEND_LIBPATH='$(HOST_PREFIX)/lib32' \
-        SKIPUTILS='MakeLangId,Makensisw,NSIS Menu,zip2exe' \
-        install
-    $(INSTALL) -m755 '$(BUILD_TOOLS_PREFIX)/bin/makensis' '$(BUILD_TOOLS_PREFIX)/bin/$(MXE_TOOL_PREFIX)makensis'
-endef
-else
-define $(PKG)_BUILD
-    cd '$(1)' && scons VERBOSE=1 \
-        PATH='$(PATH)' \
-        MINGW_CROSS_PREFIX='$(MXE_TOOL_PREFIX)' \
-        PREFIX='$(BUILD_TOOLS_PREFIX)' \
-        `[ -d /usr/local/include ] && echo APPEND_CPPPATH=/usr/local/include` \
-        `[ -d /usr/local/lib ]     && echo APPEND_LIBPATH=/usr/local/lib` \
-        SKIPUTILS='MakeLangId,Makensisw,NSIS Menu,zip2exe' \
-        install
-    $(INSTALL) -m755 '$(BUILD_TOOLS_PREFIX)/bin/makensis' '$(BUILD_TOOLS_PREFIX)/bin/$(MXE_TOOL_PREFIX)makensis'
-endef
+ifeq ($(ENABLE_WINDOWS_64),yes)
+    $(PKG)_PREBUILD = \
+        $(SED) -i 's/pei-i386/pei-x86-64/' '$(1)/SCons/Config/linker_script'
 endif
+
+define $(PKG)_BUILD
+    $($(PKG)_PREBUILD)
+    cd '$(1)' && scons VERBOSE=1 \
+        PATH='$(PATH)' \
+        MINGW_CROSS_PREFIX='$(MXE_TOOL_PREFIX)' \
+        PREFIX='$(BUILD_TOOLS_PREFIX)' \
+        SKIPPLUGINS='System' \
+        SKIPUTILS='MakeLangId,Makensisw,NSIS Menu,zip2exe' \
+        NSIS_MAX_STRLEN=8192 \
+        install
+    $(INSTALL) -m755 '$(BUILD_TOOLS_PREFIX)/bin/makensis' '$(BUILD_TOOLS_PREFIX)/bin/$(MXE_TOOL_PREFIX)makensis'
+endef
