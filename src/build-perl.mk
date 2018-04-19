087c7b5eabde6685d0b1612896cbc51499af2104
John W. Eaton <jwe@octave.org>
2018-04-19 17:21:35 -0400

    build-perl: new package

---
+++ src/build-perl.mk
@@ -0,0 +1,25 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := build-perl
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 5.26.1
+$(PKG)_CHECKSUM := 
+$(PKG)_SUBDIR   := perl-$($(PKG)_VERSION)
+$(PKG)_FILE     := perl-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://www.cpan.org/src/5.0/$($(PKG)_FILE)
+
+$(PKG)_DEPS     := 
+
+ifeq ($(MXE_WINDOWS_BUILD),yes)
+  define $(PKG)_BUILD
+  endef
+else
+  define $(PKG)_BUILD
+    cd '$(1)' && $($(PKG)_CONFIGURE_ENV) './Configure' -s -d -e -Dprefix='$(HOST_PREFIX)' \
+        -D $(CONFIGURE_CPPFLAGS) -D $(CONFIGURE_LDFLAGS) \
+        && $(CONFIGURE_POST_HOOK)
+
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install DESTDIR='$(3)'
+  endef
+endif
