4c06ce7c6c244ffb25aef211e4c45bba0e8db0a1
John W. Eaton <jwe@octave.org>
2018-04-20 17:33:02 -0400

    build-perl, perl: build shared libperl

--- src/build-perl.mk
+++ src/build-perl.mk
@@ -8,17 +8,23 @@ $(PKG)_CHECKSUM := 74a0822429508d593513a0dfd6f51a907bad68d0
 $(PKG)_SUBDIR   := perl-$($(PKG)_VERSION)
 $(PKG)_FILE     := perl-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.cpan.org/src/5.0/$($(PKG)_FILE)
-
 $(PKG)_DEPS     := 
+
+ifeq ($(BUILD_SHARED),yes)
+  $(PKG)_CONFIGURE_ARGS := -Duseshrplib
+endif
 
 ifeq ($(MXE_WINDOWS_BUILD),yes)
   define $(PKG)_BUILD
   endef
 else
   define $(PKG)_BUILD
-    cd '$(1)' && $($(PKG)_CONFIGURE_ENV) './Configure' -s -d -e -Dprefix='$(HOST_PREFIX)' \
+    cd '$(1)' \
+      && $($(PKG)_CONFIGURE_ENV) './Configure' -s -d -e \
+        -Dprefix='$(HOST_PREFIX)' \
         -D $(CONFIGURE_CPPFLAGS) -D $(CONFIGURE_LDFLAGS) \
-        && $(CONFIGURE_POST_HOOK)
+        $($(PKG)_CONFIGURE_ARGS) \
+      && $(CONFIGURE_POST_HOOK)
 
     $(MAKE) -C '$(1)' -j '$(JOBS)' install DESTDIR='$(3)'
   endef
