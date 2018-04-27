47f9cfc9fbb25c94d86ab8a503c7e55976cf663a
John W. Eaton <jwe@octave.org>
2018-04-27 11:03:09 -0400

    stable-octave: bump version to 4.3.92

--- src/stable-octave.mk
+++ src/stable-octave.mk
@@ -6,7 +6,7 @@
 
 PKG             := stable-octave
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.3.91
+$(PKG)_VERSION  := 4.3.92
 $(PKG)_CHECKSUM := ## No checksum
 $(PKG)_SUBDIR   := octave-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.lz
