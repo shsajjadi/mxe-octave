62dea4da5be80c4cca6a2d9f6d65b5002233df8e
John Donoghue <John Donoghue@localhost>
2018-05-29 09:25:59 -0400

    * src/libidn2.mk: update to v2.0.5

--- src/libidn2.mk
+++ src/libidn2.mk
@@ -3,8 +3,8 @@
 
 PKG             := libidn2
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.0.4
-$(PKG)_CHECKSUM := b00490f2251f218b130628eccacc170a4bb49db5
+$(PKG)_VERSION  := 2.0.5
+$(PKG)_CHECKSUM := 91a5122511f87aaaa3f1b700f23c487aaf0dbd9e
 $(PKG)_SUBDIR   := libidn2-$($(PKG)_VERSION)
 $(PKG)_FILE     := libidn2-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := ftp://ftp.gnu.org/gnu/libidn/$($(PKG)_FILE)
