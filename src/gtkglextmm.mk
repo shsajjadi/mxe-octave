8d05cde5773a1d997889537a07388fb73282feb9
John Donoghue <John Donoghue@localhost>
2018-06-01 12:02:05 -0400

    more gnome repo -> github updates
    * src/gtkglext.mk, src/gtkglextmm.mk, src/libxml++.mk:
      update update rule to use githib

--- src/gtkglextmm.mk
+++ src/gtkglextmm.mk
@@ -11,11 +11,9 @@ $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/gtkglext/gtkglextmm/$($(
 $(PKG)_DEPS     := gtkglext gtkmm2
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://git.gnome.org/cgit/gtkglextmm/refs/tags' | \
-    grep '<a href=' | \
-    $(SED) -n "s,.*<a href='[^']*/tag/?h=\\([0-9][^']*\\)'.*,\\1,p" | \
-    grep -v '1\.1\.' | \
-    head -1
+    $(WGET) -q -O- https://github.com/GNOME/gtkglextmm/tags | \
+    $(SED) -n 's|.*releases/tag/\([^"]*\).*|\1|p' | grep -v '^1\.1\.' | $(SORT) -V | \
+    tail -1
 endef
 
 define $(PKG)_BUILD
