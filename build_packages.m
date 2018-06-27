b9b365590f7741b79ffa8d857014e7b36d3ba18a
John Donoghue <John Donoghue@localhost>
2018-06-27 14:46:53 -0400

    of-stk: update to v2.5.1
    * src/of-stk.mk: update version/checksum
    
    * src/of-stk.mk: update stk version

--- build_packages.m
+++ build_packages.m
@@ -55,7 +55,7 @@ unwind_protect
   try_install instrument-control-0.3.1.tar.gz
   try_install generate_html-0.3.1.tar.gz
   try_install financial-0.5.1.tar.gz
-  try_install stk-2.4.2.tar.gz
+  try_install stk-2.5.1.tar.gz
   try_install splines-1.3.2.tar.gz
   try_install dataframe-1.2.0.tar.gz
   try_install lssa-0.1.2.tar.gz
