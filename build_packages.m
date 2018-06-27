25b525a96360ecbfe427e0ec2ce447d11cc538f0
John Donoghue <John Donoghue@localhost>
2018-06-27 16:15:35 -0400

    of-image: update to v2.8.0
    * src/of-image.mk: update version, checksum
    
    * build_packages.m: update image version

--- build_packages.m
+++ build_packages.m
@@ -35,7 +35,7 @@ unwind_protect
   try_install control-3.1.0.tar.gz
   try_install signal-1.4.0.tar.gz
   try_install communications-1.2.1.tar.gz
-  try_install image-2.6.2.tar.gz
+  try_install image-2.8.0.tar.gz
   try_install io-2.4.11.tar.gz
   try_install statistics-1.4.0.tar.gz
   try_install geometry-3.0.0.tar.gz
