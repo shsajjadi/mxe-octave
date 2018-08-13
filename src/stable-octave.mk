82a2df08cae59600fb0e199d90b94dd9a7e18bef
John W. Eaton <jwe@octave.org>
2018-08-13 13:31:44 -0400

    stable-octave: update version numbers

--- src/stable-octave.mk
+++ src/stable-octave.mk
@@ -13,8 +13,8 @@
 
 PKG             := stable-octave
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.4.1-rc2
-$(PKG)_WIN_VERSION := 4.4.0.92
+$(PKG)_VERSION  := 4.4.1
+$(PKG)_WIN_VERSION := 4.4.1.90
 $(PKG)_CHECKSUM := ## No checksum
 $(PKG)_SUBDIR   := octave-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.lz
