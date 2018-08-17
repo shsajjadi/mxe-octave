c0c20553bb63f7e658591252768e690720c2b1a3
Rik <rik@octave.org>
2018-08-17 13:57:05 -0700

    fc_update.bat: Remove all cache files before rebuilding (bug #54405).
    * fc_update.bat: Use "del" to delete all files in fontconfig cache before
    proceeding.

--- installer-files/fc_update.bat
+++ installer-files/fc_update.bat
@@ -1,5 +1,8 @@
 @echo off
 
+REM Remove contents of cache directory including tmp and lock files (bug #45458)
+del /Q /S %LOCALAPPDATA%\fontconfig\cache\ >nul 2>&1
+
 set ROOT_DIR=%~dp0
 if EXIST %ROOT_DIR%\mingw32\bin\octave-cli.exe set ROOT_DIR=%ROOT_DIR%\mingw32
 if EXIST %ROOT_DIR%\mingw64\bin\octave-cli.exe set ROOT_DIR=%ROOT_DIR%\mingw64
