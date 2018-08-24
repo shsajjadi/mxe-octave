e9770f2a918d139874b999c07a5f2976ea23f7d5
John Donoghue <John Donoghue@localhost>
2018-08-24 12:52:01 -0400

    Add MSYS2_PKG_UPDATE rule and use in msys2 packages
    * Makefile.in: add  MSYS2_PKG_UPDATE def
    
    * src/msys2-bash.mk, src/msys2-coreutils.mk, src/msys2-diffutils.mk,
     src/msys2-dos2unix.mk, src/msys2-filesystem.mk, src/msys2-findutils.mk,
     src/msys2-gawk.mk, src/msys2-gcc-libs.mk, src/msys2-gmp.mk,
     src/msys2-gnupg.mk, src/msys2-grep.mk, src/msys2-gzip.mk,
     src/msys2-icu.mk, src/msys2-info.mk, src/msys2-keyring.mk,
     src/msys2-less.mk, src/msys2-libassuan.mk, src/msys2-libbz2.mk,
     src/msys2-libcrypt.mk, src/msys2-libexpat.mk, src/msys2-libffi.mk,
     src/msys2-libgcrypt.mk, src/msys2-libgnutls.mk, src/msys2-libgpg-error.mk,
     src/msys2-libgpgme.mk, src/msys2-libhogweed.mk, src/msys2-libiconv.mk,
     src/msys2-libidn2.mk, src/msys2-libintl.mk, src/msys2-libksba.mk,
     src/msys2-liblzma.mk, src/msys2-libmetalink.mk, src/msys2-libnettle.mk,
     src/msys2-libnpth.mk, src/msys2-libopenssl.mk, src/msys2-libp11-kit.mk,
     src/msys2-libpcre.mk, src/msys2-libpsl.mk, src/msys2-libreadline.mk,
     src/msys2-libsqlite.mk, src/msys2-libtasn1.mk, src/msys2-libunistring.mk,
     src/msys2-libutil-linux.mk, src/msys2-libxml2.mk, src/msys2-make.mk,
     src/msys2-mintty.mk, src/msys2-mpfr.mk, src/msys2-ncurses.mk,
     src/msys2-pacman-mirrors.mk, src/msys2-pacman.mk, src/msys2-patch.mk,
     src/msys2-perl.mk, src/msys2-runtime.mk, src/msys2-sed.mk,
     src/msys2-tar.mk, src/msys2-unzip.mk, src/msys2-wget.mk,
     src/msys2-zip.mk, src/msys2-zlib.mk: use MSYS2_PKG_UPDATE

--- src/msys2-libxml2.mk
+++ src/msys2-libxml2.mk
@@ -13,14 +13,10 @@ $(PKG)_SUBDIR   :=
 $(PKG)_FILE     := $($(PKG)_NAME)-$($(PKG)_VERSION)-$(MSYS2_ARCH).pkg.tar.xz
 $(PKG)_URL      := $(MSYS2_URL)/$($(PKG)_FILE)/download
 
-#https://sourceforge.net/projects/msys2/files/REPOS/MSYS2/x86_64/bash-4.4.019-3-x86_64.pkg.tar.xz/download
-
 $(PKG)_DEPS     := 
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- '$(MSYS2_BASE_URL)/' | \
-    $(SED) -n 's,.*title="$($(PKG)_NAME)-\([0-9][^"]*\)".*,\1,p' | \
-    head -1
+    $(MSYS2_PKG_UPDATE)
 endef
 
 define $(PKG)_BUILD
