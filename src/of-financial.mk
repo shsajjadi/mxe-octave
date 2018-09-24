e1252d41dc3dd5d7197a62659fda5bbd294e4421
John Donoghue <John Donoghue@localhost>
2018-09-24 12:34:45 -0400

    of-financial: update to 0.5.2
    *  build_packages.m: update finacial version
    
    *  src/of-financial.mk: update version, checksum

--- src/of-financial.mk
+++ src/of-financial.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-financial
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.5.1
-$(PKG)_CHECKSUM := 9c2c2603f488571421951810c9b0524fe0ad1b6b
+$(PKG)_VERSION  := 0.5.2
+$(PKG)_CHECKSUM := 7e56690e9cf90fb7d6d837c9f16f32e1bf580ac3
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := financial-$($(PKG)_VERSION)
 $(PKG)_FILE     := financial-$($(PKG)_VERSION).tar.gz
