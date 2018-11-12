361a61617bb523b613d5624eb426660c130ef8f1
John Donoghue <John Donoghue@localhost>
2018-11-12 10:59:34 -0500

    of-queueing: update to 1.2.6
    * build_packages.m: update queueing version number
    
    * src/of-queueing.mk: update version, checksum

--- build_packages.m
+++ build_packages.m
@@ -59,7 +59,7 @@ unwind_protect
   try_install splines-1.3.2.tar.gz
   try_install dataframe-1.2.0.tar.gz
   try_install lssa-0.1.3.tar.gz
-  try_install queueing-1.2.5.tar.gz
+  try_install queueing-1.2.6.tar.gz
   try_install nurbs-1.3.13.tar.gz
   try_install strings-1.2.0.tar.gz
   try_install ga-0.10.0.tar.gz
