5687f961443c00e1caff8dcba3eb08893cc97c71
John Donoghue <John Donoghue@localhost>
2018-12-12 08:22:38 -0500

    mpc: update to v1.1.0
    * src/gcc-mpc.mk, src/mpc.mk: update version, checksum, downlaod urls

--- src/gcc-mpc.mk
+++ src/gcc-mpc.mk
@@ -3,12 +3,11 @@
 
 PKG             := gcc-mpc
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.2
-$(PKG)_CHECKSUM := 5072d82ab50ec36cc8c0e320b5c377adb48abe70
+$(PKG)_VERSION  := 1.1.0
+$(PKG)_CHECKSUM := b019d9e1d27ec5fb99497159d43a3164995de2d0
 $(PKG)_SUBDIR   := mpc-$($(PKG)_VERSION)
 $(PKG)_FILE     := mpc-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://www.multiprecision.org/mpc/download/$($(PKG)_FILE)
-$(PKG)_URL_2    := http://ftp.debian.org/debian/pool/main/m/mpclib/mpclib_$($(PKG)_VERSION).orig.tar.gz
+$(PKG)_URL      := https://ftp.gnu.org/gnu/mpc/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc-gmp gcc-mpfr
 
 define $(PKG)_UPDATE
