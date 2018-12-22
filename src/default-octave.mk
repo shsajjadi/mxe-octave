9290a30f3e6be4641bea887ec1025b8e73c61f2e
Markus M?tzel <markus.muetzel@gmx.de>
2018-12-22 10:01:16 +0100

    default-octave.mk: Update for version 6.0.0 (bug #55262).

--- src/default-octave.mk
+++ src/default-octave.mk
@@ -13,8 +13,8 @@
 
 PKG             := default-octave
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.0.0
-$(PKG)_WIN_VERSION  := $($(PKG)_VERSION)
+$(PKG)_VERSION  := 6.0.0
+$(PKG)_WIN_VERSION := $($(PKG)_VERSION)
 $(PKG)_CHECKSUM := ## No checksum
 $(PKG)_SUBDIR   := octave-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.lz
