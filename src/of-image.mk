011f9958e8cc6ce2ec48be497272aa2598c0f754
John Donoghue <John Donoghue@localhost>
2019-01-02 10:04:32 -0500

    of-image: update to v2.10.0
    * build_packages.m: update image version
    
    * src/of-image.mk: update version, checksum, remove depends

--- src/of-image.mk
+++ src/of-image.mk
@@ -3,13 +3,13 @@
 
 PKG             := of-image
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.8.1
-$(PKG)_CHECKSUM := 623c4c9a7cb928613fb3dfa6884211f10a2ffb65
+$(PKG)_VERSION  := 2.10.0
+$(PKG)_CHECKSUM := c614fb5c35ea5f4c07ade33d0458ccc9a763b366
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := image-$($(PKG)_VERSION)
 $(PKG)_FILE     := image-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := '$(OCTAVE_FORGE_BASE_URL)/$($(PKG)_FILE)/download'
-$(PKG)_DEPS     := of-signal
+$(PKG)_DEPS     := 
 
 ifeq ($(ENABLE_BINARY_PACKAGES),yes)
     $(PKG)_DEPS += $(OCTAVE_TARGET)
