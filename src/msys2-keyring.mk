39b5a64ee317a4c89c52eb87c7ae7e9ef0dc31f9
John Donoghue <john.donoghue@ieee.org>
2019-01-19 08:23:46 -0500

    * src/msys2-keyring.mk: fix checksum variable chain

--- src/msys2-keyring.mk
+++ src/msys2-keyring.mk
@@ -6,8 +6,8 @@ $(PKG)_NAME     := msys2-keyring
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := r9.397a52e-1
 # the keyring package is the same for both systems
-$(PKG)_x86_64_CS := e99da023381c02f8ce036cfca85bb6d82d9e8fa0
-$(PKG)_CS       := $($(PKG)_x86_64_CS
+$(PKG)_x86_64_CS := 846109181a3d49def1da5a422be5d80c4ecb14df
+$(PKG)_CS       := $($(PKG)_x86_64_CS)
 $(PKG)_CHECKSUM := $($(PKG)_CS)
 $(PKG)_SUBDIR   := 
 $(PKG)_FILE     := $($(PKG)_NAME)-$($(PKG)_VERSION)-any.pkg.tar.xz
