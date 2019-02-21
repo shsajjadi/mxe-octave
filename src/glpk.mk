d41a5f444ca364cccc7b6e3136fb3387236ba783
John Donoghue <John Donoghue@localhost>
2019-02-21 13:56:46 -0500

    glpk: update to 4.65
    * src/glpk-1-fixes.patch: new patch
    
    * src/glpk.mk: update version, checksum

--- src/glpk.mk
+++ src/glpk.mk
@@ -3,12 +3,12 @@
 
 PKG             := glpk
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.60
-$(PKG)_CHECKSUM := 4e55a3d2aa38b19c5bdd8a195738dbaf44e91e90
+$(PKG)_VERSION  := 4.65
+$(PKG)_CHECKSUM := d9b607a9cf3a25b754a0cd9a842ea5043f8604db
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := glpk-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := ftp://ftp.gnu.org/gnu/glpk/$($(PKG)_FILE)
-$(PKG)_DEPS     := gmp
+$(PKG)_DEPS     := gmp suitesparse
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://ftp.gnu.org/gnu/glpk/?C=M;O=D' | \
@@ -18,6 +18,7 @@ endef
 
 define $(PKG)_BUILD
     mkdir '$(1)/.build'
+    cd '$(1)' && autoreconf -fi -I m4
     cd '$(1)/.build' && '$(1)/configure' \
         $(CONFIGURE_CPPFLAGS) $(CONFIGURE_LDFLAGS) \
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
