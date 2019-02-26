cbe279d95821e9f80197fa8cf21e7e6b922dd8f1
John Donoghue <John Donoghue@localhost>
2019-02-26 07:57:02 -0500

    * src/curl.mk: update to 7.64.0

--- src/curl.mk
+++ src/curl.mk
@@ -3,8 +3,8 @@
 
 PKG             := curl
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 7.63.0
-$(PKG)_CHECKSUM := b87cbac60d20998967001995f409239a74b702b4
+$(PKG)_VERSION  := 7.64.0
+$(PKG)_CHECKSUM := 7539acc0742c2fb1472bc2904f0bd58eeebc011a
 $(PKG)_SUBDIR   := curl-$($(PKG)_VERSION)
 $(PKG)_FILE     := curl-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://curl.haxx.se/download/$($(PKG)_FILE)
