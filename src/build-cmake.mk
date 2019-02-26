3c874683b69d6c37c9c0513460f32dddc5fdd2ea
John Donoghue <John Donoghue@localhost>
2019-02-26 08:15:58 -0500

    cmake: update to v3.13.4
    * src/build-cmake.mk, src/cmake.mk: update version, checksum

--- src/build-cmake.mk
+++ src/build-cmake.mk
@@ -3,8 +3,8 @@
 
 PKG             := build-cmake
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.13.2
-$(PKG)_CHECKSUM := 101b8599b5ddd4df9127e227c3fe13ed3d8e266a
+$(PKG)_VERSION  := 3.13.4
+$(PKG)_CHECKSUM := 242ac4986ab38ddb06c7bfb7eb950e4c1b1ca770
 $(PKG)_SUBDIR   := cmake-$($(PKG)_VERSION)
 $(PKG)_FILE     := cmake-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.cmake.org/files/v$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
