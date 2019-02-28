f43b68d945121bb1cb3f3c370d4edbac74917ff7
John Donoghue <John Donoghue@localhost>
2019-02-28 12:47:25 -0500

    * src/geos.mk: update to 3.7.1

--- src/geos.mk
+++ src/geos.mk
@@ -3,8 +3,8 @@
 
 PKG             := geos
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.6.2
-$(PKG)_CHECKSUM := b9d0578c6b5308cf05a3dd8873802f9862c04291
+$(PKG)_VERSION  := 3.7.1
+$(PKG)_CHECKSUM := c06a2e7620b9197c6b9ac456395f4ee4fed9cb86
 $(PKG)_SUBDIR   := geos-$($(PKG)_VERSION)
 $(PKG)_FILE     := geos-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://download.osgeo.org/geos/$($(PKG)_FILE)
@@ -22,10 +22,6 @@ define $(PKG)_BUILD
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         $(ENABLE_SHARED_OR_STATIC) \
         --prefix='$(HOST_PREFIX)'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
-
-    '$(MXE_CC)' \
-        -W -Wall -Werror -ansi -pedantic \
-        '$(2).c' -o '$(HOST_BINDIR)/test-geos.exe' \
-        -lgeos_c `'$(HOST_BINDIR)/geos-config' --cflags --libs` -lstdc++
+    $(MAKE) -C '$(1)' -j '$(JOBS)' $(MXE_DISABLE_PROGS) $(MXE_DISABLE_DOCS)
+    $(MAKE) -C '$(1)' -j 1 install $(MXE_DISABLE_PROGS) $(MXE_DISABLE_DOCS) DESTDIR='$(3)'
 endef
