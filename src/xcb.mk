8ee4b411b68edccdb02147f823793347e95997d4
John Donoghue <John Donoghue@localhost>
2019-03-11 15:27:33 -0400

    * src/xcb.mk: update to v1.13.1

--- src/xcb.mk
+++ src/xcb.mk
@@ -3,12 +3,19 @@
 
 PKG             := xcb
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.12
-$(PKG)_CHECKSUM := 2f03490d1c75c8a3f902f74b717af6501773926a
+$(PKG)_VERSION  := 1.13.1
+$(PKG)_CHECKSUM := 2ab17a1bb2a44e0a9cb0b26bcf899689ba1bbb3b
 $(PKG)_SUBDIR   := libxcb-$($(PKG)_VERSION)
 $(PKG)_FILE     := libxcb-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.x.org/archive/individual/xcb/$($(PKG)_FILE)
 $(PKG)_DEPS     := pthread-stubs util-macros xau xcb-proto
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'https://www.x.org/archive/individual/xcb/' | \
+    $(SED) -n 's,.*<a href="libxcb-\([0-9\.]*\)\.tar.gz".*,\1,p' | \
+    $(SORT) -V |
+    tail -1
+endef
 
 ifeq ($(MXE_WINDOWS_BUILD),yes)
   define $(PKG)_BUILD
