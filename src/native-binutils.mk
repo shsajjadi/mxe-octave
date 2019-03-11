53b60cc07fde56833c109b94bd3f4db875a9f6a0
John Donoghue <John Donoghue@localhost>
2019-03-11 15:27:26 -0400

    binutils: update to v2.32
    * src/build-binutils.mk, src/native-binutils.mk:
      update version, checksum

--- src/native-binutils.mk
+++ src/native-binutils.mk
@@ -3,8 +3,8 @@
 
 PKG             := native-binutils
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.29.1
-$(PKG)_CHECKSUM := 172244a349d07ec205c39c0321cbc354c125e78e
+$(PKG)_VERSION  := 2.32
+$(PKG)_CHECKSUM := cd45a512af1c8a508976c1beb4f5825b3bb89f4d
 $(PKG)_SUBDIR   := binutils-$($(PKG)_VERSION)
 $(PKG)_FILE     := binutils-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/binutils/$($(PKG)_FILE)
