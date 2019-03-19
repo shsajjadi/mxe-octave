9f03cff6b74f68131df23cf7036469bb06c4b376
John Donoghue <John Donoghue@localhost>
2019-03-19 10:34:36 -0400

    qt5: update to v5.12.2
    * src/qtbase.mk: update version, checksum
    
    * src/qtimageformats.mk, src/qtsvg.mk, src/qttools.mk
      : update checksum

--- src/qtimageformats.mk
+++ src/qtimageformats.mk
@@ -4,7 +4,7 @@
 PKG             := qtimageformats
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION   = $(qtbase_VERSION)
-$(PKG)_CHECKSUM := f28b8715c7cd1390f4ea3a45ac68ee1901a242a5
+$(PKG)_CHECKSUM := 841ada24b4f57a359f25456a887b737665638b18
 $(PKG)_SUBDIR    = $(subst qtbase,qtimageformats,$(qtbase_SUBDIR))
 $(PKG)_FILE      = $(subst qtbase,qtimageformats,$(qtbase_FILE))
 $(PKG)_URL       = $(subst qtbase,qtimageformats,$(qtbase_URL))
