397a32afd01d0089f53dc2d62f7d1ce4e2846881
John Donoghue <John Donoghue@localhost>
2019-04-17 07:51:07 -0400

    of-optim: update to v1.6.0
    * dist-files.mk: removed ref to patch file
    
    * src/of-optim.mk: update version, checksum
    
    * src/of-optim-1-fixes.patch: removed
    
    * src/of-optim-2-deprecated.patch: removed

--- src/of-optim.mk
+++ src/of-optim.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-optim
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.5.3
-$(PKG)_CHECKSUM := 3633a5257fbf882e1d654303f9e3ce090456165a
+$(PKG)_VERSION  := 1.6.0
+$(PKG)_CHECKSUM := b7b55449a2861f0d318db1ccf1c37294fcdca604
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := optim-$($(PKG)_VERSION)
 $(PKG)_FILE     := optim-$($(PKG)_VERSION).tar.gz
