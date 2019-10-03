e9402e2940ce69a10f394e6a15a826015a9f4716
John Donoghue <John Donoghue@localhost>
2019-10-03 15:26:47 -0400

    * src/gnutls.mk: update to v3.6.10

--- src/gnutls.mk
+++ src/gnutls.mk
@@ -2,8 +2,8 @@
 # See index.html for further information.
 
 PKG             := gnutls
-$(PKG)_VERSION  := 3.6.9
-$(PKG)_CHECKSUM := 4a12757b129562ae92a01ca890ed282050595296
+$(PKG)_VERSION  := 3.6.10
+$(PKG)_CHECKSUM := c073c6b0c57506a592854471576321be80f809d4
 $(PKG)_SUBDIR   := gnutls-$($(PKG)_VERSION)
 $(PKG)_FILE     := gnutls-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.gnutls.org/gcrypt/gnutls/v3.6/$($(PKG)_FILE)
