edc171e61c96bb10ba8fa026793dd57effd09567
John Donoghue <John Donoghue@localhost>
2020-01-29 08:34:50 -0500

    * installer-files/post-install.bat: create qt.conf (Bug #57679)

--- installer-files/post-install.bat
+++ installer-files/post-install.bat
@@ -15,11 +15,27 @@ if %MSYSTEM%==MSYS (
  set OCTAVE_HOME=%ROOT_DIR%
 ) else (
  if %MSYSTEM%==MINGW32 (
- set OCTAVE_HOME=%ROOT_DIR%\mingw32
+ set OCTAVE_HOME=%ROOT_DIR%mingw32
 ) else (
- set OCTAVE_HOME=%ROOT_DIR%\mingw64
+ set OCTAVE_HOME=%ROOT_DIR%mingw64
 ))
 
+Rem QT subdir path
+IF EXIST "%OCTAVE_HOME%\qt5" (
+  set QT_SUBDIR=qt5/
+) ELSE (
+  set QT_SUBDIR=
+)
+
+Rem Create qt.conf
+set QT_CONF=%OCTAVE_HOME%\bin\qt.conf
+echo [Paths] > %QT_CONF%
+echo Prefix=%OCTAVE_HOME:\=/% >> %QT_CONF%
+echo Documentation=%QT_SUBDIR%doc >> %QT_CONF%
+echo Headers=%QT_SUBDIR%include >> %QT_CONF%
+echo Libraries=%QT_SUBDIR%libs >> %QT_CONF%
+echo Plugins=%QT_SUBDIR%plugins >> %QT_CONF%
+echo Translations=%QT_SUBDIR%translations >> %QT_CONF%
 
 Rem run bash to to regitser the initial envorinment
 echo "Setting up MSYS system ..."
