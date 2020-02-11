d75e33bc4b1396290218d61f88f2944f904028d6
John Donoghue <John Donoghue@localhost>
2020-02-11 13:11:40 -0500

    * src/python-sympy.mk: downgrade to v1.4, remove old files when installing

--- src/python-sympy.mk
+++ src/python-sympy.mk
@@ -2,8 +2,8 @@
 # See index.html for further information.
 
 PKG             := python-sympy
-$(PKG)_VERSION  := 1.5
-$(PKG)_CHECKSUM := be2e740860f7900f0ee2a8102d2943fded44125c
+$(PKG)_VERSION  := 1.4
+$(PKG)_CHECKSUM := 9485daf9e29f4ffa20e04111bea940a917eb3a69
 $(PKG)_SUBDIR   := 
 $(PKG)_FILE     := sympy-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/sympy/sympy/releases/download/sympy-$($(PKG)_VERSION)/$($(PKG)_FILE)
@@ -14,6 +14,8 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
+    rm -rf '$(HOST_PREFIX)/python/sympy*'
+
     mkdir -p '$(3)$(HOST_PREFIX)/python'
     cd '$(1)/sympy-$($(PKG)_VERSION)' && tar cf - sympy | ( cd '$(3)$(HOST_PREFIX)/python'; tar xpf - )
     cd '$(1)' && tar cf - --exclude=sympy-$($(PKG)_VERSION)/sympy . | ( cd '$(3)$(HOST_PREFIX)/python'; tar xpf - )
