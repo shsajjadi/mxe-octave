1d33382f988f13f1fada7fb35605a5cd21a7a332
John Donoghue <John Donoghue@localhost>
2020-03-03 13:28:30 -0500

    * src/eigen.mk: update to v3.3.7

--- src/eigen.mk
+++ src/eigen.mk
@@ -3,9 +3,9 @@
 
 PKG             := eigen
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.0.17
-$(PKG)_CHECKSUM := 7e1674420a8eef7e90e1875ef5b9e828fb9db381
-$(PKG)_SUBDIR   := $(PKG)-$(PKG)-b23437e61a07
+$(PKG)_VERSION  := 3.3.7
+$(PKG)_CHECKSUM := 743c1dc00c6680229d8cc87d44debe5a71d15c01
+$(PKG)_SUBDIR   := $(PKG)-$(PKG)-323c052e1731
 $(PKG)_FILE     := $($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://bitbucket.org/$(PKG)/$(PKG)/get/$($(PKG)_FILE)
 $(PKG)_DEPS     :=
@@ -18,7 +18,8 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && \
-    cmake . -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+    mkdir '$(1).build'
+    cd '$(1).build' && \
+    cmake -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' '$(1)'
+    $(MAKE) -C '$(1).build' -j '$(JOBS)' DESTDIR='$(3)' install
 endef
