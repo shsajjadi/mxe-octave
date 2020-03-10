871a55b8cb43b0151127de74bd7d7c8e8a1dc305
John Donoghue <John Donoghue@localhost>
2020-03-10 17:06:13 -0400

    of-dicom: update to v0.3.0
    * src/of-dicom-1-gdcm3.patch: remove patch
    
    * dist-files.mk: update version/checksum
    
    * dist-files.mk: remove patch ref

--- src/of-dicom.mk
+++ src/of-dicom.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-dicom
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.2.2
-$(PKG)_CHECKSUM := fb86c93098b6d163c499d3cf8bbd3ee381180904
+$(PKG)_VERSION  := 0.3.0
+$(PKG)_CHECKSUM := c9cf943df76a6cf6469542b2be3c34f0904bd751
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := dicom-$($(PKG)_VERSION)
 $(PKG)_FILE     := dicom-$($(PKG)_VERSION).tar.gz
