48871fac87f7512e664c8dfa2ffb7bc9fbc53bcb
John Donoghue <john.donoghue@ieee.org>
2020-03-21 13:55:19 -0400

    gcc: update to v9.3.0
    * src/build-gcc.mk, src/native-gcc.mk: update version, checksum

--- src/native-gcc.mk
+++ src/native-gcc.mk
@@ -3,8 +3,8 @@
 
 PKG             := native-gcc
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 9.2.0
-$(PKG)_CHECKSUM := 306d27c3465fa36862c206738d06d65fff5c3645
+$(PKG)_VERSION  := 9.3.0
+$(PKG)_CHECKSUM := b746688bf045a316fc92c3528138ad10d0822b6b
 $(PKG)_SUBDIR   := gcc-$($(PKG)_VERSION)
 $(PKG)_FILE     := gcc-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/gcc/gcc-$($(PKG)_VERSION)/$($(PKG)_FILE)
