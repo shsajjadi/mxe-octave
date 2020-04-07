b844e7e455ce3e9d5ec12c633dc0ccfe43ea9b9b
John Donoghue <john.donoghue@ieee.org>
2020-04-07 08:44:22 -0400

    * src/msys2-pacman-mirrors.mk: use a separate i686 checksum

--- src/msys2-pacman-mirrors.mk
+++ src/msys2-pacman-mirrors.mk
@@ -5,9 +5,10 @@ PKG             := msys2-pacman-mirrors
 $(PKG)_NAME     := pacman-mirrors
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 20200307-1
-# the mirror package is the same for both systems
+# the mirror package is NOT the same for both systems
+$(PKG)_i686_CS  := 98a92b638e7544ac981a0bfaef0ae428be62153a
 $(PKG)_x86_64_CS := 06583c1512e0422a8210d4ed69ed93526874fafd
-$(PKG)_CS       := $($(PKG)_x86_64_CS)
+$(PKG)_CS       := $($(PKG)_$(MSYS2_ARCH)_CS)
 $(PKG)_CHECKSUM := $($(PKG)_CS)
 $(PKG)_SUBDIR   := 
 $(PKG)_FILE     := $($(PKG)_NAME)-$($(PKG)_VERSION)-any.pkg.tar.xz
