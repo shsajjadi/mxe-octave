db3b2e382c66e8afcca46d755214aa5b4c852494
John Donoghue <john.donoghue@ieee.org>
2020-04-11 11:02:18 -0400

    * src/build-sed.mk: update to v4.8

--- src/build-sed.mk
+++ src/build-sed.mk
@@ -3,8 +3,8 @@
 
 PKG             := build-sed
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.7
-$(PKG)_CHECKSUM := dbc842f6fdf538f366d97e7c4b4f241eaefd14a5
+$(PKG)_VERSION  := 4.8
+$(PKG)_CHECKSUM := 61bd770062d49cdab3f0f45df473e2bf950ba266
 $(PKG)_SUBDIR   := sed-$($(PKG)_VERSION)
 $(PKG)_FILE     := sed-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/sed/$($(PKG)_FILE)
