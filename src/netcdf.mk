2fc423b965fad2dc8cd76038727b806b48b3496c
Markus M?tzel <markus.muetzel@gmx.de>
2020-05-30 16:17:02 +0200

    * src/netcdf.mk: Include lib directory in LDFLAGS.

--- src/netcdf.mk
+++ src/netcdf.mk
@@ -37,8 +37,8 @@ define $(PKG)_BUILD
         --prefix='$(HOST_PREFIX)' \
         $($(PKG)_CONFIGURE_OPTIONS) \
 	&& $($(PKG)_CONFIGURE_POST_HOOK)
-    $(MAKE) -C '$(1)' -j '$(JOBS)' LDFLAGS='-no-undefined'
-    $(MAKE) -C '$(1)' -j 1 install LDFLAGS='-no-undefined' $(MXE_DISABLE_PROGS) $(MXE_DISABLE_DOCS) DESTDIR='$(3)'
+    $(MAKE) -C '$(1)' -j '$(JOBS)' LDFLAGS='-no-undefined -L$(HOST_LIBDIR)'
+    $(MAKE) -C '$(1)' -j 1 install LDFLAGS='-no-undefined -L$(HOST_LIBDIR)' $(MXE_DISABLE_PROGS) $(MXE_DISABLE_DOCS) DESTDIR='$(3)'
   
     if [ ! "x$(MXE_NATIVE_BUILD)" = "xyes" ]; then \
       $(LN_SF) '$(HOST_BINDIR)/nc-config' '$(BUILD_TOOLS_PREFIX)/bin/$(MXE_TOOL_PREFIX)nc-config'; \
