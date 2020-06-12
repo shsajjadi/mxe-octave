1cb32f0b54d65ed7640231b36191020d6e149762
John Donoghue <john.donoghue@ieee.org>
2020-06-12 08:00:52 -0400

    * installer-files/cmdshell.bat: allow path set in .bat file ti teh transfers through to bash

--- installer-files/cmdshell.bat
+++ installer-files/cmdshell.bat
@@ -1,6 +1,7 @@
 @echo off
 
 set OCTAVE_HOME=%~dp0
+Rem NOTE: OCTAVE_HOME has a end \
 Rem convert to 8.3 format
 for %%I in ("%OCTAVE_HOME%") do set OCTAVE_HOME=%%~sI
 
@@ -15,14 +16,14 @@ set MSYSDIR=%OCTAVE_HOME%
 set MSYSTEM=MSYS
 
 Rem if no msys-1.0, must be msys2
-if NOT EXIST %OCTAVE_HOME%\bin\msys-1.0.dll set MSYSDIR=%OCTAVE_HOME%\usr
+if NOT EXIST %OCTAVE_HOME%bin\msys-1.0.dll set MSYSDIR=%OCTAVE_HOME%usr
 
 Rem 32 or 64 bit
-if EXIST %OCTAVE_HOME%\mingw32\bin\octave-cli.exe set MSYSTEM=MINGW32
-if EXIST %OCTAVE_HOME%\mingw64\bin\octave-cli.exe set MSYSTEM=MINGW64
+if EXIST %OCTAVE_HOME%mingw32\bin\octave-cli.exe set MSYSTEM=MINGW32
+if EXIST %OCTAVE_HOME%mingw64\bin\octave-cli.exe set MSYSTEM=MINGW64
 
-if EXIST %OCTAVE_HOME%\mingw32\bin\octave-cli.exe set OCTAVE_HOME=%OCTAVE_HOME%\mingw32
-if EXIST %OCTAVE_HOME%\mingw64\bin\octave-cli.exe set OCTAVE_HOME=%OCTAVE_HOME%\mingw64
+if EXIST %OCTAVE_HOME%mingw32\bin\octave-cli.exe set OCTAVE_HOME=%OCTAVE_HOME%mingw32\
+if EXIST %OCTAVE_HOME%mingw64\bin\octave-cli.exe set OCTAVE_HOME=%OCTAVE_HOME%mingw64\
 
 Rem   Set up PATH. Make sure the octave bin dir
 Rem   comes first.
@@ -30,6 +31,9 @@ set PATH=%OCTAVE_HOME%qt5\bin;%OCTAVE_HOME%bin;%OCTAVE_HOME%python;%PATH%
 set TERM=cygwin
 set GS=gs.exe
 set GNUTERM=wxt
+
+Rem tell msys2 to use the paths we set here which matches what octave would do
+set MSYS2_PATH_TYPE=inherit
 
 %MSYSDIR%\bin\bash.exe --login -i
 
