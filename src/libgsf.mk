c15e877df88fbdee2bfcc93dd9e105f197c4ce44
John Donoghue <john.donoghue@ieee.org>
2020-06-12 11:53:40 -0400

    * src/libgsf.mk: update to v1.14.30

--- src/libgsf.mk
+++ src/libgsf.mk
@@ -3,8 +3,8 @@
 
 PKG             := libgsf
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.14.27
-$(PKG)_CHECKSUM := b6082b71bf9d6e1cdafde9628cae58fcedc4a8fd
+$(PKG)_VERSION  := 1.14.30
+$(PKG)_CHECKSUM := 5eb15d574c6b9e9c5e63bbcdff8f866b3544485a
 $(PKG)_SUBDIR   := libgsf-$($(PKG)_VERSION)
 $(PKG)_FILE     := libgsf-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://ftp.gnome.org/pub/gnome/sources/libgsf/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
@@ -23,19 +23,23 @@ endef
 
 define $(PKG)_BUILD
     echo 'Libs.private: -lz -lbz2'          >> '$(1)'/libgsf-1.pc.in
-    $(SED) -i 's,^\(Requires:.*\),\1 gio-2.0,' '$(1)'/libgsf-1.pc.in
+    #$(SED) -i 's,^\(Requires:.*\),\1 gio-2.0,' '$(1)'/libgsf-1.pc.in
+    $(SED) -i 's,\ssed\s, $(SED) ,g'           '$(1)'/gsf/Makefile.in
     cd '$(1)' && ./configure \
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         $(ENABLE_SHARED_OR_STATIC) \
         --prefix='$(HOST_PREFIX)' \
         --disable-nls \
         --disable-gtk-doc \
+	--without-python \
         --with-zlib \
         --with-bz2 \
+	--without-gpio \
         PKG_CONFIG='$(MXE_PKG_CONFIG)' \
 	PKG_CONFIG_PATH='$(HOST_LIBDIR)/pkgconfig' \
 	&& $(CONFIGURE_POST_HOOK)
 
-    $(MAKE) -C '$(1)'     -j '$(JOBS)'
-    $(MAKE) -C '$(1)'     -j 1         install
+    $(MAKE) -C '$(1)'     -j 1 install-pkgconfigDATA DESTDIR='$(3)'
+    $(MAKE) -C '$(1)/gsf'     -j '$(JOBS)' 
+    $(MAKE) -C '$(1)/gsf' -j 1 install $(MXE_DISABLE_DOCS) $(MXE_DISABLE_PROGS) DESTDIR='$(3)'
 endef
