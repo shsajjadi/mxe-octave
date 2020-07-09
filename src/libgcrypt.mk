9511d5a4da86e313fde0054a28254143b440978d
John Donoghue <john.donoghue@ieee.org>
2020-07-09 17:07:08 -0400

    * src/libgcrypt.mk: update to v1.8.6

--- src/libgcrypt.mk
+++ src/libgcrypt.mk
@@ -3,8 +3,8 @@
 
 PKG             := libgcrypt
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.8.5
-$(PKG)_CHECKSUM := 2d8781e92f88706707a1e76fb628b499ad538a30
+$(PKG)_VERSION  := 1.8.6
+$(PKG)_CHECKSUM := 406b02873833427898d16bcc8483bc5c91c73d4a
 $(PKG)_SUBDIR   := libgcrypt-$($(PKG)_VERSION)
 $(PKG)_FILE     := libgcrypt-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := ftp://ftp.gnupg.org/gcrypt/libgcrypt/$($(PKG)_FILE)
