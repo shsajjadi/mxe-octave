e0840d1a1e4640e1c32b96984d850393454c1c80
John Donoghue <john.donoghue@ieee.org>
2020-07-12 12:23:18 -0400

    * src/freetds.mk: update to v1.2.3

--- src/freetds.mk
+++ src/freetds.mk
@@ -3,8 +3,8 @@
 
 PKG             := freetds
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.2.2
-$(PKG)_CHECKSUM := ae4482ea96c2f71acfc4cf1581fe09460d968e34
+$(PKG)_VERSION  := 1.2.3
+$(PKG)_CHECKSUM := 7c59f0a2161c45a3f46e5145dd49a072ea427553
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := ftp://ftp.freetds.org/pub/$(PKG)/stable/$($(PKG)_FILE)
