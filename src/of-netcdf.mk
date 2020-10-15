89647c113fecded97d20a2794854a6e93b63aae2
John Donoghue <john.donoghue@ieee.org>
2020-10-15 11:40:57 -0400

    * src/of-netcdf.mk: update to v1.0.14

--- src/of-netcdf.mk
+++ src/of-netcdf.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-netcdf
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.13
-$(PKG)_CHECKSUM := 026c8e8a62a20a1fc1ff2fbb1faef5a2bd25c6f0
+$(PKG)_VERSION  := 1.0.14
+$(PKG)_CHECKSUM := 46c84940f54b99d68f879720bcf6c827fa1a54f9
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := netcdf-$($(PKG)_VERSION)
 $(PKG)_FILE     := netcdf-$($(PKG)_VERSION).tar.gz
