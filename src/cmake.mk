072d6a56365a8dda26a926b8a27421d946c888b8
John Donoghue <john.donoghue@ieee.org>
2020-10-19 08:05:58 -0400

    * src/build-cmake.mk, src/cmake.mk: update to v3.18.4

--- src/cmake.mk
+++ src/cmake.mk
@@ -3,8 +3,8 @@
 
 PKG             := cmake
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.18.3
-$(PKG)_CHECKSUM := 94c15d9a3bf3f1842f4eec97276441083013d30c
+$(PKG)_VERSION  := 3.18.4
+$(PKG)_CHECKSUM := 73ab5348c881f1a53c250b66848b6ee101c9fe1f
 $(PKG)_SUBDIR   := cmake-$($(PKG)_VERSION)
 $(PKG)_FILE     := cmake-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.cmake.org/files/v$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
