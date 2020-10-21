bf80fa59ebf50d8e0125aa090e6eeec600b568b6
Markus M?tzel <markus.muetzel@gmx.de>
2020-10-21 16:24:44 +0200

    * src/gdal.mk: Use parallel make.

--- src/gdal.mk
+++ src/gdal.mk
@@ -74,7 +74,7 @@ define $(PKG)_BUILD
         --without-ruby \
         --without-python \
         LIBS="-ljpeg `'$(MXE_PKG_CONFIG)' --libs libtiff-4`"
-    $(MAKE) -C '$(1)'       -j 1 lib-target
+    $(MAKE) -C '$(1)'       -j '$(JOBS)' lib-target
     $(MAKE) -C '$(1)'       -j 1 install-lib
     $(MAKE) -C '$(1)/port'  -j 1 install
     $(MAKE) -C '$(1)/gcore' -j 1 install
