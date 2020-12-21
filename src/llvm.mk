993d34e9fb0e98d285e799a2fddbfd26c2596c37
Markus M?tzel <markus.muetzel@gmx.de>
2020-12-21 17:36:07 +0100

    LLVM: Update to version 10.0.1.
    * src/llvm.mk: Update version and checksum.

--- src/llvm.mk
+++ src/llvm.mk
@@ -3,8 +3,8 @@
 
 PKG             := llvm
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 9.0.1
-$(PKG)_CHECKSUM := f7fcf3bd92d130784513c06efe6910f135372ce3
+$(PKG)_VERSION  := 10.0.1
+$(PKG)_CHECKSUM := 25d07260f3b7bf4f647e115c4a663fdeda130fbd
 $(PKG)_SUBDIR   := llvm-$($(PKG)_VERSION).src
 $(PKG)_FILE     := llvm-$($(PKG)_VERSION).src.tar.xz
 $(PKG)_URL      := https://github.com/llvm/llvm-project/releases/download/llvmorg-$($(PKG)_VERSION)/$($(PKG)_FILE)
