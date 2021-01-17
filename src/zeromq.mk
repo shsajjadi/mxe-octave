053268fa7af9b6f999ea8bdafc6622e3ddb264ef
John Donoghue <john.donoghue@ieee.org>
2021-01-17 08:00:16 -0500

    zeromq: update to v4.3.3
    * src/zeromq.mk: update version/checksum
    
    * src/mingw-zeromq-1-fixes.patch: new file
    
    * dist-files: add ref to patch file

--- src/zeromq.mk
+++ src/zeromq.mk
@@ -3,8 +3,8 @@
 
 PKG             := zeromq
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.3.1
-$(PKG)_CHECKSUM := 6cce22d830eaf95feff7cab00744df13ad7ab7f3
+$(PKG)_VERSION  := 4.3.3
+$(PKG)_CHECKSUM := d78bc504194d6908df40a2b9e41849b181b02491
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://github.com/$(PKG)/libzmq/releases/download/v$($(PKG)_VERSION)/$($(PKG)_FILE)
@@ -20,8 +20,10 @@ define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         --prefix='$(HOST_PREFIX)' \
+        --disable-perf \
         --without-libsodium \
         $(ENABLE_SHARED_OR_STATIC) 
+
     $(MAKE) -C '$(1)' -j '$(JOBS)'
-    $(MAKE) -C '$(1)' -j 1 install
+    $(MAKE) -C '$(1)' -j 1 install $(MXE_DISABLE_PROGS) $(MXE_DISABLE_DOCS) DESTDIR='$(3)'
 endef
