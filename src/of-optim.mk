840036572cf478760ac14cc4da87fea942b3d2a9
John Donoghue <john.donoghue@ieee.org>
2021-02-17 07:41:51 -0500

    * src/of-optim.mk: update to v1.6.1

--- src/of-optim.mk
+++ src/of-optim.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-optim
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.6.0
-$(PKG)_CHECKSUM := b7b55449a2861f0d318db1ccf1c37294fcdca604
+$(PKG)_VERSION  := 1.6.1
+$(PKG)_CHECKSUM := 3dd4efab7acb47f815c73ad4050114d00e1daf42
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := optim-$($(PKG)_VERSION)
 $(PKG)_FILE     := optim-$($(PKG)_VERSION).tar.gz
