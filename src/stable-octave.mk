eb80c498baca109d929e6b01d89ac2a671e22c94
John W. Eaton <jwe@octave.org>
2021-02-19 15:04:48 -0500

    update version number for Octave 6.2 release
    * src/stable-octave.mk: Update version to 6.2.1
    * src/release-octave.mk: Update version to 6.2.0.  Update URL and
    checksum for release.

--- src/stable-octave.mk
+++ src/stable-octave.mk
@@ -13,7 +13,7 @@
 
 PKG             := stable-octave
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 6.1.91
+$(PKG)_VERSION  := 6.2.1
 $(PKG)_CHECKSUM := ## No checksum
 $(PKG)_SUBDIR   := octave-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.lz
