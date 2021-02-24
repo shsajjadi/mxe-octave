4a0e0dfdf99df2acb82c3ee01f073572c144ee49
John Donoghue <john.donoghue@ieee.org>
2021-02-24 07:42:54 -0500

    * src/of-instrument-control.mk: update to v0.7.0

--- src/of-instrument-control.mk
+++ src/of-instrument-control.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-instrument-control
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.6.0
-$(PKG)_CHECKSUM := 6c7138e874f7bf7f8d6cd44d62b31aeecbde314d
+$(PKG)_VERSION  := 0.7.0
+$(PKG)_CHECKSUM := b2d6c091dccea01389c51a9279b385135ce035d6
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := instrument-control-$($(PKG)_VERSION)
 $(PKG)_FILE     := instrument-control-$($(PKG)_VERSION).tar.gz
