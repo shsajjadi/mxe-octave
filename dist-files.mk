aa5077917faf6cd0d499f1b12d4e11f4a4c31a3b
Markus M?tzel <markus.muetzel@gmx.de>
2021-04-05 12:41:03 +0200

    texinfo: Support makeinfo being installed on path with non-ASCII characters on Windows.
    * src/mingw-texinfo-2-makeinfo-non-ASCII-perl.patch: Add new patch.
    * dist-files.mk: Add new file to list.

--- dist-files.mk
+++ dist-files.mk
@@ -349,6 +349,7 @@ SRC_FILES_1 := \
   mingw-portaudio-1-pkgconfig.patch \
   mingw-s2tc-1-fixes.patch \
   mingw-texinfo-1-fixes.patch \
+  mingw-texinfo-2-makeinfo-non-ASCII-perl.patch \
   mingw-utils-1-portability-fix.patch \
   mingw-w64-1-float-h.patch \
   mingw-w64.mk \
